993fd1657c129c03dcc0ae892d423a7bca6c9509
Joshua Mitchell <joshua.mitchell@portlandoregon.gov>
2018-03-16 17:02:34 -0700

    Small updates from Composer rebuild.

--- web/modules/contrib/panels/src/Form/PanelsAddBlockForm.php
+++ web/modules/contrib/panels/src/Form/PanelsAddBlockForm.php
@@ -62,7 +62,7 @@ class PanelsAddBlockForm extends PanelsBlockConfigureFormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL, $tempstore_id = NULL, $machine_name = NULL, $block_id = NULL) {
+  public function buildForm(array $form, FormStateInterface $form_state, $tempstore_id = NULL, $machine_name = NULL, $block_id = NULL, Request $request = NULL) {
     $form = parent::buildForm($form, $form_state, $tempstore_id, $machine_name, $block_id);
     $form['region']['#default_value'] = $request->query->get('region');
     return $form;
