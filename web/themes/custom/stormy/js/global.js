f3a856e2391b0f61eb4be2e9156ec640022b3e91
Joshua Mitchell <joshua.mitchell@portlandoregon.gov>
2018-05-07 15:29:47 -0700

    POWR-96 Create Barrio subtheme and share back to repo for Brittany to try.

---
+++ web/themes/custom/stormy/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.stormy = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
