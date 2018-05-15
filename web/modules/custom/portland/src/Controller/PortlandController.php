6bac6e72db0fb2bf924d70fc184e394ccf14658e
gclapp <gregory.clapp@portlandoregon.gov>
2018-05-15 16:47:21 -0700

    POWR-165 Hide local actions from Group content page (/group/*/nodes); we don't want the Relate node or Create node links visible there, but do want Add member to still appear in Members tab. Misc code cleanup.

--- web/modules/custom/portland/src/Controller/PortlandController.php
+++ web/modules/custom/portland/src/Controller/PortlandController.php
@@ -25,22 +25,6 @@ class PortlandController extends GroupContentController {
      * {@inheritdoc}
      */
     public function addPage(GroupInterface $group, $create_mode = FALSE) {
-        $test = \Drupal::service('entity_field.manager')->getFieldDefinitions('node', 'my_custom_content');
-
-        $entity_manager = \Drupal::entityManager();
-        $ids = null;
-        // $test2 = $entity_manager
-        //     ->getStorage($entity_manager
-        //     ->getEntityTypeFromClass(get_called_class()))
-        //     ->load('service_location');
-
-        // $entity = \Drupal::entityManager();
-
-        // $types = \Drupal::entityTypeManager()
-        //     ->getStorage('node')
-        //     ->loadMultiple();
-
-        // $defs = DefaultPluginManager::getDefinitions();
 
         $build = GroupContentController::addPage($group, $create_mode);
 
@@ -49,24 +33,28 @@ class PortlandController extends GroupContentController {
             return $build;
         }
 
-        // // Overwrite the label and description for all of the displayed bundles.
-        // $storage_handler = $this->entityTypeManager->getStorage('node_type');
-        // foreach ($this->addPageBundles($group, $create_mode) as $plugin_id => $bundle_name) {
-        //     if (!empty($build['#bundles'][$bundle_name])) {
-        //         $plugin = $group->getGroupType()->getContentPlugin($plugin_id);
-        //         $bundle_label = $storage_handler->load($plugin->getEntityBundle())->label();
-        //         $bundle_id = $storage_handler->load($plugin->getEntityBundle())->id();
-        //         $bundle_entity_bundle = $storage_handler->load($plugin->getEntityBundle())->entity_bundle();
+        // Overwrite the label and description for all of the displayed bundles.
+        $storage_handler = $this->entityTypeManager->getStorage('node_type');
+        $page_bundles = $this->addPageBundles($group, $create_mode);
+        // NOTE: ksort is working here, but bundle types are still displayed out of order.
+        // there must be some other sorting process that occurs after this point.
+        ksort($page_bundles);
+        foreach ($page_bundles as $plugin_id => $bundle_name) {
+            if (!empty($build['#bundles'][$bundle_name])) {
+                $plugin = $group->getGroupType()->getContentPlugin($plugin_id);
+                $bundle_label = $storage_handler->load($plugin->getEntityBundle())->label();
+                $bundle_id = $storage_handler->load($plugin->getEntityBundle())->id();
+                $bundle_desc = \Drupal::config('node.type.' . $bundle_id)->get('description');
 
-        //         $t_args = ['%node_type' => $bundle_label];
-        //         $description = $create_mode
-        //         ? $this->t('Create a node of type %node_type in the group...', $t_args)
-        //         : $this->t('Add an existing node of type %node_type to the group...', $t_args);
+                $t_args = ['%node_type' => $bundle_label];
+                $description = $create_mode
+                ? $this->t('Create a node of type %node_type in the group...<br>' . $bundle_desc, $t_args)
+                : $this->t('Add an existing node of type %node_type to the group...<br>' . $bundle_desc, $t_args);
 
-        //         $build['#bundles'][$bundle_name]['label'] = $bundle_label;
-        //         $build['#bundles'][$bundle_name]['description'] = $description;
-        //     }
-        // }
+                $build['#bundles'][$bundle_name]['label'] = $bundle_label;
+                $build['#bundles'][$bundle_name]['description'] = $description;
+            }
+        }
 
         return $build;
     }
