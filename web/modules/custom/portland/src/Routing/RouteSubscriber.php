2f948dda0978d64154424e06e83eaa94f854e331
gclapp <gregory.clapp@portlandoregon.gov>
2018-05-15 16:47:21 -0700

    POWR-165 Hide local actions from Group content page (/group/*/nodes); we don't want the Relate node or Create node links visible there, but do want Add member to still appear in Members tab. Misc code cleanup.

--- web/modules/custom/portland/src/Routing/RouteSubscriber.php
+++ web/modules/custom/portland/src/Routing/RouteSubscriber.php
@@ -10,25 +10,16 @@ use Symfony\Component\Routing\RouteCollection;
  */
 class RouteSubscriber extends RouteSubscriberBase {
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function alterRoutes(RouteCollection $collection) {
-
-    // override route entity.group_content.group_node_add_page from Group module.
-    // we want this to instead load a page that allows user to select a group content type,
-    // but in our case, we want the content type descriptions to be displayed.
-    // this changes path '/group/{group}/node/create' to '/group/{group}/content/add'
-    if ($route = $collection->get('entity.group_content.group_node_add_page')) {
-        $route->setPath('/group/{group}/add-content');
-        $route->setDefault('_controller', '\Drupal\portland\Controller\PortlandController::addPage');
+    /**
+     * {@inheritdoc}
+     */
+    protected function alterRoutes(RouteCollection $collection) {
+        // override route entity.group_content.group_node_add_page from Group module.
+        // we want this to instead load a page that allows user to select a group content type,
+        // but in our case, we want the content type descriptions to be displayed.
+        if ($route = $collection->get('entity.group_content.group_node_add_page')) {
+            $route->setDefault('_controller', '\Drupal\portland\Controller\PortlandController::addPage');
+        }
     }
-
-    // // Always deny access to '/user/logout'.
-    // // Note that the second parameter of setRequirement() is a string.
-    // if ($route = $collection->get('user.logout')) {
-    //   $route->setRequirement('_access', 'FALSE');
-    // }
-  }
 
 }
