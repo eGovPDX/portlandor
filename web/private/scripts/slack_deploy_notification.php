6aae17ad8cda7441696ef3ce6c811b179e3595d8
kxwang <xinju.wang@portlandoregon.gov>
2018-06-18 12:02:54 -0700

    POWR-227 Always show text

--- web/private/scripts/slack_deploy_notification.php
+++ web/private/scripts/slack_deploy_notification.php
@@ -152,7 +152,7 @@ function _get_secrets($requiredKeys, $defaults)
 /**
  * Send a notification to slack
  */
-function _slack_notification($slack_url, $channel, $username, $text, $attachment, $alwaysShowText = false)
+function _slack_notification($slack_url, $channel, $username, $text, $attachment, $alwaysShowText = true)
 {
   $attachment['fallback'] = $text;
   $post = array(
