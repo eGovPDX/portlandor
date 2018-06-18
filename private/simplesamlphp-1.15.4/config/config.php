dd9bfe9befa904a1f11e2f5466cc6302bea21d84
Greg Clapp <gregory.clapp@portlandoregon.gov>
2018-06-18 20:41:28 +0000

    POWR-202: incremental update. Can get to welcome page if module.php page has a hard-coded value set in ['PATH_INFO']. Var doesn't get set otherwise. Fast-cgi config issue?

--- private/simplesamlphp-1.15.4/config/config.php
+++ private/simplesamlphp-1.15.4/config/config.php
@@ -1015,7 +1015,7 @@ $config = array(
      *
      * (This option replaces the old 'session.handler'-option.)
      */
-    'store.type'                    => 'memcache',
+    'store.type'                    => 'phpsession',
 
     /*
      * The DSN the sql datastore should connect to.
