562beb06fcaf0cbfa918347044362f929eb560e3
Greg Clapp <gregory.clapp@portlandoregon.gov>
2018-06-27 21:24:21 +0000

    POWR-238a removed _get_secrets function since it's already defined

--- web/private/simplesamlphp-1.15.4/config/config.php
+++ web/private/simplesamlphp-1.15.4/config/config.php
@@ -1053,27 +1053,4 @@ $config = array(
 );
 
 
-/**
- * Get secrets from secrets file.
- *
- * @param array $requiredKeys  List of keys in secrets file that must exist.
- */
-function _get_secrets($requiredKeys, $defaults)
-{
-  $secretsFile = $_ENV['HOME'] . '/files/private/secrets.json';
-  if (!file_exists($secretsFile)) {
-    die('No secrets file found. Aborting!');
-  }
-  $secretsContents = file_get_contents($secretsFile);
-  $secrets = json_decode($secretsContents, 1);
-  if ($secrets == FALSE) {
-    die('Could not parse json in secrets file. Aborting!');
-  }
-  $secrets += $defaults;
-  $missing = array_diff($requiredKeys, array_keys($secrets));
-  if (!empty($missing)) {
-    die('Missing required keys in json secrets file: ' . implode(',', $missing) . '. Aborting!');
-  }
-  return $secrets;
-}
 
