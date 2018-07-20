ad6e02510273cb0278cf60176717d97b680d6a64
kxwang <xinju.wang@portlandoregon.gov>
2018-07-19 17:04:27 -0700

    POWR-262 Media path update. Add media tab. Group media view. Paragraphs.

--- web/modules/custom/portland/src/Routing/RouteSubscriber.php
+++ web/modules/custom/portland/src/Routing/RouteSubscriber.php
@@ -4,11 +4,20 @@ namespace Drupal\portland\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
 use Symfony\Component\Routing\RouteCollection;
+use Drupal\Core\Routing\RoutingEvents;
 
 /**
  * Listens to the dynamic route events.
  */
 class RouteSubscriber extends RouteSubscriberBase {
+    public static function getSubscribedEvents() {
+        // Come after field_ui.
+        $events[RoutingEvents::ALTER] = array(
+          'onAlterRoutes',
+          3100,
+        );
+        return $events;
+      }
 
     /**
      * {@inheritdoc}
@@ -19,6 +28,9 @@ class RouteSubscriber extends RouteSubscriberBase {
         // but in our case, we want the content type descriptions to be displayed.
         if ($route = $collection->get('entity.group_content.group_node_add_page')) {
             $route->setDefault('_controller', '\Drupal\portland\Controller\PortlandController::addPage');
+        }
+        else if ($route = $collection->get('entity.group_content.group_media_add_page')) {
+            $route->setDefault('_controller', '\Drupal\portland\Controller\PortlandMediaController::addPage');
         }
     }
 
