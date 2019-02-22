f88c863e838822ca762ac212d1809315787ce663
gclapp <gregory.clapp@portlandoregon.gov>
2019-02-22 14:42:09 -0800

    POWR-772 - merged new filter code from a temp branch, updated description and comments, put the media view mode names in consts.

--- web/modules/custom/portland/modules/portland_media_embed_helper/src/Plugin/Filter/PortlandMediaEmbedHelperAutoDisplayMode.php
+++ web/modules/custom/portland/modules/portland_media_embed_helper/src/Plugin/Filter/PortlandMediaEmbedHelperAutoDisplayMode.php
@@ -12,11 +12,14 @@ use Drupal\filter\Render\FilteredMarkup;
  * @Filter(
  *   id = "portland_media_embed_helper_auto_mode_filter",
  *   title = @Translation("Portland Media Embed Helper Auto Display Mode Filter"),
- *   description = @Translation("Adds classes to entity embed containers that correspond to the selected media browser"),
+ *   description = @Translation("Sets the display mode for embedded images automatically based on whether they are right-aligned or full width."),
  *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
  * )
  */
 class PortlandMediaEmbedHelperAutoDisplayMode extends FilterBase {
+
+  const DISPLAY_MODE_RIGHT = "embedded_50";
+  const DISPLAY_MODE_FULL = "embedded_100";
 
   /**
    * Processes text in the following ways: 
@@ -31,17 +34,10 @@ class PortlandMediaEmbedHelperAutoDisplayMode extends FilterBase {
   {
     $result = new FilterProcessResult($text);
 
-    // data-align attribute will either be set to "right" or not included at all for full width
-
-    // QUESTION: do we want to use the Embedded100 and Embedded50 display modes for videos as well?
-
     // spin through all drupal-entity elements with data embed button values
     // for each button...
     //   if data-align is set and set to "right," and type is image, set display mode to Embedded50
     //   if data-align is set and type is image, set display mode to Embedded100
-
-
-
     if (stristr($text, 'data-embed-button') !== false) {
       $dom = Html::load($text);
       $xpath = new \DOMXPath($dom);
@@ -51,9 +47,9 @@ class PortlandMediaEmbedHelperAutoDisplayMode extends FilterBase {
 
         if ($embed_button == "image_browser") {
           if (!is_null($alignment) && $alignment == "right") {
-            $display = "embedded50";
+            $display = DISPLAY_MODE_RIGHT;
           } else {
-            $display = "embedded100";
+            $display = DISPLAY_MODE_FULL;
           }
           // change value of data-entity-embed-display="view_mode:media.embedded"
           $node->setAttribute("data-entity-embed-display", "view_mode:media." . $display);
