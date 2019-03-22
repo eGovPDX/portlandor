94b6c22ec2b8b61e5e3339f359aa9785a666b2c1
gclapp <gregory.clapp@portlandoregon.gov>
2019-03-22 11:08:24 -0700

    POWR-842 - try using language from DrupalContext to log in user

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -8,23 +8,36 @@ use Behat\Behat\Context\SnippetAcceptingContext;
  *
  * @codingStandardsIgnoreStart
  */
-class FeatureContext extends DrupalContext implements SnippetAcceptingContext {
+class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
 
     /**
    * @Given I am logged in as user :name
    */
   public function iAmLoggedInAsUser($name) {
-    // Get the suffix for the site based on the environment
-    $site_name = (getenv('CIRCLE_BRANCH') == "master") ? "dev" : getenv('CIRCLE_BRANCH');
 
-    // Generate the link to login. Ignore stderr output
-    $uli = shell_exec("terminus drush portlandor.$site_name uli $name");
+    $manager = $this->getUserManager();
 
-    // Trim EOL characters.
-    $uli = trim($uli);
+    // Change internal current user.
+    $manager->setCurrentUser($manager->getUser($name));
 
-    // Log in.
-    $this->getSession()->visit($uli);
+    // Login.
+    $this->login($manager->getUser($name));
+
+    // // Get the suffix for the site based on the environment
+    // $site_name = (getenv('CIRCLE_BRANCH') == "master") ? "dev" : getenv('CIRCLE_BRANCH');
+
+    // // Generate the link to login. Ignore stderr output
+    // $uli = shell_exec("terminus drush portlandor.$site_name uli $name");
+
+    // // Trim EOL characters.
+    // $uli = trim($uli);
+
+    // // Log in.
+    // $this->getSession()->visit($uli);
+
+    // $driver = $this->getDriver();
+    // $manager = $this->getUserManager();
+
   }
 
 }
