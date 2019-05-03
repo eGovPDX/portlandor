fe151d6aa80607437b85d3bb120974fe9e761b99
Joshua Mitchell <joshua.mitchell@portlandoregon.gov>
2019-05-03 10:57:49 -0700

    POWR-1012 fix order of operations in quicksilver script and lando tools

--- web/private/scripts/drush_post_deploy.php
+++ web/private/scripts/drush_post_deploy.php
@@ -1,9 +1,5 @@
 <?php
-// Import all config changes.
-echo "Start importing configuration from yml files...\n";
-passthru('drush config-import -y');
-echo "Done importing of configuration.\n";
-//Clear all cache
+//Clear the cache to make sure database doesn't conflict
 echo "Start rebuilding cache...\n";
 passthru('drush cr');
 echo "Done rebuilding cache.\n";
@@ -11,3 +7,15 @@ echo "Done rebuilding cache.\n";
 echo "Start applying any database updates required (as with running update.php)...\n";
 passthru('drush updatedb -y');
 echo "Done applying any database updates required.\n";
+// Import all config changes.
+echo "Start importing configuration from yml files...\n";
+passthru('drush config-import -y');
+echo "Done importing of configuration.\n";
+// Run the cron.
+echo "Starting cron...\n";
+passthru('drush core:cron -y');
+echo "Done importing of configuration.\n";
+//Clear the cache again
+echo "Start rebuilding cache...\n";
+passthru('drush cr');
+echo "Done rebuilding cache.\n";
