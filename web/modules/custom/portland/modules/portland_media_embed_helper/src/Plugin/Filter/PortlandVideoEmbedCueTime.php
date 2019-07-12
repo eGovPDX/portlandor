7353c7477c2822bc18c3e7757e44b857db4ad609
Greg Clapp <gregory.clapp@portlandoregon.gov>
2019-07-12 11:41:25 -0700

    POWR-1090 updated embed dialog to include start time, added text filter to add it to YouTube urls

---
+++ web/modules/custom/portland/modules/portland_media_embed_helper/src/Plugin/Filter/PortlandVideoEmbedCueTime.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\portland_media_embed_helper\Plugin\Filter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Component\Utility\Xss;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+use Drupal\filter\Render\FilteredMarkup;
+
+/**
+ * @Filter(
+ *   id = "portland_video_embed_cue_time",
+ *   title = @Translation("Portland Video Embed Cue Time"),
+ *   description = @Translation("When enabled, applies a start cue time to YouTube and Vimeo video URLs. Requries a custom embed dialog field to capture the timecode. This filter must be processed after the Display Embedded Entities filter."),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ * )
+ */
+class PortlandVideoEmbedCueTime extends FilterBase {
+
+  /**
+   * @param [type] $text
+   * @param [type] $langcode
+   * @return void
+   */
+  public function process($text, $langcode)
+  {
+    $result = new FilterProcessResult($text);
+
+    $dom = Html::load($text);
+    $xpath = new \DOMXPath($dom);
+
+    $dom = Html::load($text);
+    $xpath = new \DOMXPath($dom);
+    // this section only gets used if data-start-cue attribute is present
+    $elems = $xpath->query("//div[@data-start-cue]");
+    foreach ($elems as $elem) {
+      $cue = $elem->getAttribute("data-start-cue");
+      $iframes = $elem->getElementsByTagName('iframe');
+      foreach ($iframes as $frame) {
+        $src = $frame->getAttribute('src');
+        $url = parse_url($src);
+        parse_str($url['query'], $query);
+        if (strpos($url['host'], 'youtube') !== false) {
+          $query['start'] = $cue;
+          $url['query'] = http_build_query($query);
+        }
+        
+        // start time doesn't currently work for Vimeo. the URL used by the embedded player doesn't seem to support the querystring var.
+        // else if (strpos($url['host'], 'vimeo') !== false) {
+        //   $url['path'] .= "#" . $cue . "s";
+        // }
+        // reassemble and set URL
+        $src = $url['scheme'] . '://' . $url['host'] . $url['path'] . '?' . $url['query'];
+        $frame->setAttribute('src', $src);
+      }
+    }
+
+    $result->setProcessedText(Html::serialize($dom))
+      ->addAttachments([
+        'library' => [
+          'filter/caption',
+        ],
+      ]);
+
+    return $result;
+  }
+}
