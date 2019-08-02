e109cdfd9ae4b23f6571ad751fd71c4315cbbfb7
Greg Clapp <gregory.clapp@portlandoregon.gov>
2019-08-01 18:09:01 -0700

    POWR-1102 custom plugin working well, can't save media as published

--- web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigrateLinkedMedia.php
+++ web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigrateLinkedMedia.php
@@ -11,7 +11,9 @@ use Drupal\Component\Utility\Xss;
 use Drupal\media\Entity\Media;
 
 /**
- * Description of what the plugin does
+ * The MigrateLinkedMedia plugin parses content and looks for images and links to documents.
+ * If found, the source URL is used to download and store the file as a media entity, and the
+ * link or image tag is converted to an entity-embed element.
  *
  * @MigrateProcessPlugin(
  *   id = "migrate_linked_media"
@@ -23,159 +25,182 @@ class MigrateLinkedMedia extends ProcessPluginBase {
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
 
-    // when run against a HTML content field during migration, the following things happen:
-
-    // * Look for all internal links (either start with "/" or "http*://portlandoregon.gov")
-    // ** Request target; check mime-type
-    // ** If HTML, try to use path for lookup if page exists in new system; create a basic link
-    // ** If document, create a media entity and replace the link tag with media-embed tag in body content
-
-    preg_match_all('/<a [^>]+>/i', $value, $result);
+    preg_match_all('/<a [^>]+>|<img [^>]+>/i', $value, $result);
     if (!empty($result[0])) {
       // load content into xpath dom so we can search it more easily
       $dom = Html::load($value);
       $xpath = new \DOMXPath($dom);
+      // do we need to deal with case sensitivity?
+      // $xpath->registerNamespacce('php', 'http://php.net/xpath');
+      // $xpath->registerPhpFunctions();
 
-      // determine download folder name (YYYY-MM)
-      $folder = date("Y-m") ;
+      // prepare download directory
+      $folder_name = date("Y-m") ;
+      $folder_uri = file_build_uri($folder_name);
       $public_path = \Drupal::service('file_system')->realpath(file_default_scheme() . "://");
-      $download_path = $public_path . "/" . $folder;
-      $result = file_prepare_directory($download_path, FILE_CREATE_DIRECTORY);
+      $download_path = $public_path . "/" . $folder_name;
+      $dir = file_prepare_directory($download_path, FILE_CREATE_DIRECTORY);
 
-      // look for links with an href
-      foreach ($xpath->query('//a[@href]') as $link) {
+      // look for links with an href and save the linked file
+      foreach ($xpath->query('//a[@href]|//img[@src]') as $link) {
 
+        // parse url from link; it will be in an href or src attribute.
+        $url = $link->getAttribute('href');
+        if (is_null($url)) {
+          $url = $link->getAttribute('src');
+        }
+
+        // if url is empty or null, skip it and continue
+        if (is_null($url) || $url == "") {
+          continue;
+        }
+
+        // if url is relative, prefix it with the POG domain
+        if (substr($url, 0, 1) == "/") {
+          $url = "https://www.portlandoregon.gov" . $url;
+        }
+
+        $url_host = parse_url($url, PHP_URL_HOST);
+
+        // if host is anything but www.portlandoregon.gov or www.portlandonline.gov,
+        // leave the link alone and continue. we don't want to download and embed
+        // external link targets.
+        if ($url_host != "www.portlandoregon.gov" && $url_host != "www.portlandonline.gov") {
+          continue;
+        } else {
+          $internal_link = true;
+        }
+
+        // download and store raw file with whatever name was in the URL, or retrieve it if it already exists
+        $file_uri = $folder_uri . '/' . pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_FILENAME);
+        if (!file_exists($file_uri)) {
+          $result = system_retrieve_file($url, $file_uri, TRUE);
+        } else {
+          $result = \Drupal::entityTypeManager()->getStorage('file')->loadByProperties(['uri' => $file_uri]);
+          if (is_array($result)) {
+            $result = reset($result);
+          }
+        }
+
+        // process the file: 
+        // delete it if it's not a type we want, or rename it with a more descriptive filename
+        // and appropriate extension. when we saved the raw file from POG, it was saved with
+        // whatever name was in the URL (typically the content id).
+
+        $absolute_path = \Drupal::service("file_system")->realpath($file_uri);
+        $mime_type = mime_content_type($absolute_path);
+
+        switch ($mime_type) {
+          case "application/pdf":
+            $file_info = ["pdf", "document"]; break;
+          case "application/msword":
+            $file_info = ["doc", "document"]; break;
+          case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            $file_info = ["docx", "document"]; break;
+          case "application/vnd.ms-excel":
+            $file_info = ["xls", "document"]; break;
+          case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            $file_info = ["xlsx", "document"]; break;
+          case "application/vnd.ms-powerpoint":
+            $file_info = ["ppt", "document"]; break;
+          case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+            $file_info = ["pptx", "document"]; break;
+          case "image/jpeg":
+            $file_info = ["jpg", "image"]; break;
+          case "image/gif":
+            $file_info = ["gif", "image"]; break;
+          case "image/png":
+            $file_info = ["png", "image"]; break;
+          case "text/html":
+            $file_info = ["html", null]; break;
+          default:
+            $file_info = null; break;
+        }
+
+        // // TODO: if url is relative and mime type is html, do we need to update the url to be from POG, or just keep it relative?
+        if ($file_info[0] == "html" && $internal_link) {
+          $link->setAttribute("href", $url);
+        }
+
+        // if ext is a type we have not defined, log it, delete the file, and continue the loop.
+        if (is_null($file_info[1])) {
+          $message = "Undefined download encountered in migration: " . $mime_type . "<br>File path: " . $absolute_path . "<br>From URL: " . $url;
+          \Drupal::logger('portland_migrations')->notice($message);
+          $result->delete();
+          continue;
+        }
+
+        // use link text as file name.
         $link_text = $link->textContent;
+        $filename = $link_text;
         if (function_exists('transliterate_filenames_transliteration')) {
           $filename = transliterate_filenames_transliteration($link_text, $source_langcode);
         }
 
-        // parse url from link
-        $url = $link->getAttribute('href'); // /shared/cfm/image.cfm?id=25873
-        if (substr(0, 26) != "https://portlandoregon.gov" && substr(0,125) != "https://www.portlandoregon.gov") {
-          $url = "https://www.portlandoregon.gov" . $url;
-        }
-
-        // need uri for managed files
-        $uri = file_build_uri($folder);
-
-        // download and store file
-        $orig_filename = pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_FILENAME);
-        if (!file_exists($uri . '/' . $orig_filename)) {
-          $result = system_retrieve_file($url, $uri, TRUE);
-        } else {
-          $files = \Drupal::entityTypeManager()->getStorage('file')->loadByProperties(['uri' => $uri]);
-          $result = $files;
-        }
-        if (!$result) continue;
-        
-        $absolute_path = \Drupal::service("file_system")->realpath($uri . "/" . $result->filename->value);
-
-        // get mime type
-        $mime_type = mime_content_type($absolute_path); // application/pdf
-
-        // determine extension from mime type
-        switch ($mime_type) {
-          case "application/pdf":
-            $ext = "pdf";
-            $type = "document";
-            break;
-          case "application/msword":
-            $ext = "doc"; 
-            $type = "document";
-            break;
-          case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-            $ext = "docx"; 
-            $type = "document";
-            break;
-          case "application/vnd.ms-excel":
-            $ext = "xls"; 
-            $type = "document";
-            break;
-          case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-            $ext = "xlsx"; 
-            $type = "document";
-            break;
-          case "application/vnd.ms-powerpoint":
-            $ext = "ppt"; 
-            $type = "document";
-            break;
-          case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
-            $ext = "pptx"; 
-            $type = "document";
-            break;
-          case "image/jpeg":
-            $ext = "jpg"; 
-            $type = "image";
-            break;
-          case "image/gif":
-            $ext = "gif"; 
-            $type = "image";
-            break;
-          case "image/png":
-            $ext = "png"; 
-            $type = "image";
-            break;
-          case "text/html":
-            // it's a link to a page, delete it instead of naming it
-            $result->delete();
-            continue 2;
-          default:
-            // other, log it
-            $message = "Unhandled download encountered in migration: " . $mime_type . "<br>File path: " . $absolute_path . "<br>From URL: " . $url;
-            \Drupal::logger('portland_migrations')->notice($message);
-            continue 2;
-        }
-
-        // rename file if it doesn't already exist.
-        // TODO: if it does exist, delete the temp file and don't rename.
+        // rename file if renamed file doesn't alredy exist
         $file = \Drupal\file\Entity\File::load($result->fid->value);
-        $new_filename = $folder . "/" . $filename . "." . $ext;
+        $new_filename = $folder_name . "/" . $filename . "." . $file_info[0];
         $stream_wrapper = \Drupal::service('file_system')->uriScheme($file->getFileUri());
         $new_filename_uri = "{$stream_wrapper}://{$new_filename}";
-        file_move($file, $new_filename_uri);
+        if (!file_exists($new_filename_uri)) {
+          $move_result = file_move($file, $new_filename_uri);
+        } else {
+          // renamed file already exists. delete temp file, load existing renamed file, and 
+          // reference that in the embed tag.
+          $file->delete();
+          $existing_files = \Drupal::entityTypeManager()->getStorage('file')->loadByProperties(['uri' => $folder_uri . '/' . $filename . '.' . $file_info[0]]);
+          $file = reset($existing_files); // should only ever be 1 file returned
+        }
 
-        // create media object for file (image or document)
-        if ($type == "document") {
+        // create media entity for file (image or document)
+        if ($file_info[1] == "document") {
           $media = Media::create([
             'bundle' => 'document',
-            'uid' => \Drupal::currentUser()->id(),
+            'uid' => 1,
             'langcode' => \Drupal::languageManager()->getDefaultLanguage()->getId(),
             'name' => $link_text,
+            'status' => 1,
             'field_document' => [
               'target_id' => $file->id()
             ],
           ]);
-          $media->setPublished();
-          $media->save();
+        } else if ($file_info[1] == "image") {
+          $media = Media::create([
+            'bundle' => 'image',
+            'uid' => 1,
+            'langcode' => \Drupal::languageManager()->getDefaultLanguage()->getId(),
+            'name' => $link_text,
+            'status' => 1,
+            'image' => [
+              'target_id' => $file->id()
+            ],
+          ]);
         }
+        $media->save();
+        $media->setPublished(TRUE)
+              ->save();
         $uuid = $media->uuid();
 
-        // replace link with entity-embed tag
-
+        // replace <a> or <img> with <entity-embed>
         $node = $dom->createElement("drupal-entity");
         $newnode = $dom->appendChild($node);
         $newnode->setAttribute("data-entity-type", "media");
         $newnode->setAttribute("data-entity-uuid", $uuid);
         $newnode->setAttribute("data-langcode", "en");
-
-        if ($type == "document") {
+        if ($file_info[1] == "document") {
           $newnode->setAttribute("data-embed-button", "document_browser");
           $newnode->setAttribute("data-entity-embed-display", "view_mode:media.embedded");
         } else {
           $newnode->setAttribute("data-embed-button", "image_browser");
           $newnode->setAttribute("data-entity-embed-display", "media_image");
         }
-
         $link->parentNode->replaceChild($newnode, $link);
       }
+      $output = Html::serialize($dom);
+      $value = $output;
     }
+
+    return $value;
   }
 
-  function portland_migrations_parseUrl($link) {
-    return $url;
-  }
-
-  function portland_migrations_getFileType($url) {
-  }
 }
