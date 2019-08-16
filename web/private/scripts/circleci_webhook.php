f21c6a6638dd858625346d783b6c0a691c80ca5c
Richard Davies <richard.davies@portlandoregon.gov>
2019-08-16 14:38:24 -0700

    POWR-1129 Make CI script compatible with Dependabot

--- web/private/scripts/circleci_webhook.php
+++ web/private/scripts/circleci_webhook.php
@@ -1,15 +1,28 @@
 <?php
 
+/*
+echo "Quicksilver Debuging Output";
+echo "\n\n";
+echo "\n========= START PAYLOAD ===========\n";
+print_r($_POST);
+echo "\n========== END PAYLOAD ============\n";
+
+echo "\n------- START ENVIRONMENT ---------\n";
+$env = $_ENV;
+foreach ($env as $key => $value) {
+  if (preg_match('#(PASSWORD|SALT|AUTH|SECURE|NONCE|LOGGED_IN)#', $key)) {
+    $env[$key] = '[REDACTED]';
+  }
+}
+print_r($env);
+echo "\n-------- END ENVIRONMENT ----------\n";
+*/
+
+
 // Original source from: https://github.com/pantheon-systems/quicksilver-examples/tree/master/webhook
 
-// Define the url the data should be sent to.
-// {wf_type} will be replaced with the workflow operation: 
-// clone_database, clear_cache, deploy, or sync_code.
-//
-// Useful to have one webhook handle multiple events.
-
 // DO NOT run tests against Live or Test environment
-if( $_ENV['PANTHEON_ENVIRONMENT'] != 'live' && $_ENV['PANTHEON_ENVIRONMENT'] != 'test') {
+if( $_ENV['PANTHEON_ENVIRONMENT'] != 'live' && $_ENV['PANTHEON_ENVIRONMENT'] != 'test' ) {
   $git_repo_name = $_ENV['PANTHEON_SITE_NAME']; // portlandor
   $git_branch_name = ($_ENV['PANTHEON_ENVIRONMENT'] === 'dev') ? 'master' : $_ENV['PANTHEON_ENVIRONMENT']; // master or powr-123
   $url = "https://circleci.com/api/v1.1/project/github/eGovPDX/$git_repo_name/tree/$git_branch_name";
@@ -39,7 +52,7 @@ if( $_ENV['PANTHEON_ENVIRONMENT'] != 'live' && $_ENV['PANTHEON_ENVIRONMENT'] != 
   curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-  print("\n==== Posting to Webhook URL ====\n");
+  print("\n==== Posting to Webhook URL $url ====\n");
   $result = curl_exec($ch);
   // print("RESULT: $result");
   print("\n===== Post Complete! =====\n");
