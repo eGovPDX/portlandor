d258e643c393c48ff9cd7ba73ef09b3e763983b0
Greg Clapp <gregory.clapp@portlandoregon.gov>
2019-08-28 12:46:40 -0700

    POWR-1332 added code to migrate 3rd level policies as children of 2nd level in taxonomy and link them to content items.

--- web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigratePolicyCategories.php
+++ web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigratePolicyCategories.php
@@ -25,6 +25,75 @@ class MigratePolicyCategories extends ProcessPluginBase {
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
  
+    // $value is an array
+    // 0 - 3rd level category name
+    // 1 - Policy Number, from which to parse the 2nd level category abbreviations
+    $l3_category = $value[0];
+    $policy_number = $value[1];
+    // if policy number is empty, this may be one of the overview documents; skip assigning a category
+    if (!$policy_number) {
+      $message = "No policy number provided; this must be an overview page (" . $l3_category . ").";
+      \Drupal::logger('portland_migrations')->notice($message);
+      throw new MigrateSkipProcessException();      
+    }
+    $policy_number_array = preg_split("/-/", $policy_number);
+    $l2_category_code = $policy_number_array[1];
+    $vocabulary = "policy_category";
+
+    // if policy number is empty
+
+    // see if l3 category exists
+    $term = $this->getTermByFieldValue($vocabulary, 'name', $l3_category);
+    if ($term && count($term) == 1) {
+      return reset($term)->id();
+    } else if (count($term) > 1) {
+      // more than one matching category found, whoops! log error.
+      $message = "Multiple matching categories found for " . $l3_category . " in vocabulary ". $vocabulary;
+      \Drupal::logger('portland_migrations')->notice($message);
+      throw new MigrateSkipProcessException();      
+    }
+
+    // l3 category doesn't exist, create it
+    $result = Term::create([
+      'name' => $l3_category, 
+      'vid'  => $vocabulary,
+    ])->save();
+    $term = $this->getTermByFieldValue($vocabulary, 'name', $l3_category);
+    $term = reset($term);
+
+    // get parent term by abbreviation and set it as the parent
+    $parent_term = $this->getTermByFieldValue($vocabulary, 'field_category_abbreviation', $l2_category_code);
+    if ($parent_term === false || count($parent_term) < 1) {
+      // parent not found, throw error
+      $message = "Parent term (" . $l2_category_code . ") not found for policies category term " . $l3_category . ". Cannot continue.";
+      \Drupal::logger('portland_migrations')->notice($message);
+      throw new MigrateException($message);
+    } else if (count($parent_term) > 1) {
+      // more than one parent category found with that code, throw error
+      $message = "Multiple matching parent categories with code " . $l2_category_code . " found in vocabulary " . $vocabulary . ". Cannot continue.";
+      \Drupal::logger('portland_migrations')->notice($message);
+      throw new MigrateException();      
+    }
+    $parent_term = reset($parent_term);
+    $parent_term_id = $parent_term->id();
+    $term->parent = $parent_term_id;
+    $term->save();
+
+    // TODO: categories aren't getting correctly assigned.
+
+    // return id of newly created term
+    return $term->id();
+  }
+
+  /**
+   * Load term by field value.
+   */
+  protected function getTermByFieldValue($vid, $field_name, $value) {
+    $properties = [];
+    $properties['vid'] = $vid;
+    $properties[$field_name] = $value;
+    $terms = \Drupal::entityManager()->getStorage('taxonomy_term')->loadByProperties($properties);
+    return $terms;
   }
 
 }
