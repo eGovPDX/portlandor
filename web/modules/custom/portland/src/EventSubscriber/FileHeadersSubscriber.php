5cf4a8e4795160b88ac48c51042f8f0e3e04e28b
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2019-09-25 10:16:29 -0700

    POWR-1445 Add subscriber to modify headers Add 'inline' instead of 'attachment'

---
+++ web/modules/custom/portland/src/EventSubscriber/FileHeadersSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\portland\EventSubscriber;
+
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Add custom headers.
+ */
+class FileHeadersSubscriber implements EventSubscriberInterface {
+  public function onRespond(FilterResponseEvent $event) {
+    $response = $event->getResponse();
+
+    if ($response->headers->has('content-disposition')) {
+      $header = $response->headers->get('content-disposition');
+      $parts = explode(';', $header);
+      $header = [];
+      foreach ($parts as $key => $value) {
+        if($value == 'attachment') {
+          $value = 'inline';
+        }
+        $header[] = $value;
+      }
+      $response->headers->set('content-disposition', implode(';', $header));
+    }
+  }
+
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::RESPONSE][] = array('onRespond');
+    return $events;
+  }
+}
