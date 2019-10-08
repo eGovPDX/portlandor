334ce929e969449c6303d3b0d6ff05def045c7d4
Richard Davies <richard.davies@portlandoregon.gov>
2019-10-08 10:43:50 -0700

    POWR-1451 Use preview image field as thumbnail image

--- web/modules/custom/video_embed_google_maps/src/Plugin/video_embed_field/Provider/GoogleMaps.php
+++ web/modules/custom/video_embed_google_maps/src/Plugin/video_embed_field/Provider/GoogleMaps.php
@@ -70,7 +70,21 @@ class GoogleMaps extends ProviderPluginBase {
   public function getRemoteThumbnailUrl() {
     //$url = 'https://drive.google.com/thumbnail?authuser=0&sz=w320&id=%s';
     //return sprintf($url, $this->getVideoId());
-    return "https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png";
+    //return "https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png";
+
+    // Note: Since thumbnail images are tied to maps by their embed URL, if multiple map media entities reference the same map embed URL, 
+    // all maps will share the thumbnail image of the first created map media entity.
+
+    // Load preview image
+    $preview_image_file_id = $_POST['image'][0]['fids'];  // A bit of a hack as it just blindly references first image field from POST data, but I don't know how else to do it
+    $preview_image = \Drupal::entityTypeManager()->getStorage('file')->load($preview_image_file_id);
+    
+    // Get preview image URL and transform it into a plain relative URL
+    $preview_image_uri = $preview_image->getFileUri();
+    $preview_image_relative_uri = file_url_transform_relative(file_create_url($preview_image_uri));
+
+    return $preview_image_relative_uri;
+    //$GLOBALS['base_url'] . file_url_transform_relative(file_create_url( \Drupal::entityTypeManager()->getStorage('file')->load($_POST['image'][0]['fids'])->getFileUri() ));
   }
 
 }
