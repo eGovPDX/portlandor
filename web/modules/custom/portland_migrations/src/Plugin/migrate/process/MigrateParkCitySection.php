14390a1ec419de2a80ea0efa99be6822be8d8144
Kevin Wang <xinju.wang@portlandoregon.gov>
2019-10-16 15:18:30 -0700

    POWR-1413 Migrate city section for parks

--- web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigrateParkCitySection.php
+++ web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigrateParkCitySection.php
@@ -14,37 +14,41 @@ use Drupal\taxonomy\Entity\Term;
  * Need to compare the text value and insert the term into the correct field in Park Facility.
  *
  * @MigrateProcessPlugin(
- *   id = "migrate_park_location_type"
+ *   id = "migrate_park_city_section"
  * )
  */
-class MigrateParkLocationType extends ProcessPluginBase {
+class MigrateParkCitySection extends ProcessPluginBase {
 
   private $term_name_and_id = [];
   private $term_loaded = FALSE;
+  private $pogAndPowrSectionMap = [
+    'OE' => 'East',
+    'N' => 'North',
+    'CC/NW' => 'Downtown',
+    'SE' => 'SE',
+    'NE' => 'NE',
+    'SW' => 'SW',
+    'NW' => 'NW',
+  ];
 
   /**
    * Migrates the park amenity category.
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    $this->loadVocabularies("park_location_type");
+    $this->loadVocabularies("city_section");
 
     // $value is a "|" separated string of all amenities
     // "horseshoe pit|volleyball court|picnic table|paths - unpaved|playground|paths - paved|accessible play area|splash pad"
     $array = explode ( "|", $value);
     $result = [];
     foreach($array as $term_name){
-      $result[] = $this->term_name_and_id[$this->processTermName($term_name)];
+      $result[] = $this->term_name_and_id[$this->pogAndPowrSectionMap[$term_name]];
     }
 
-    // Park is a newly added location type in POWR. If the result is empty, we set Park as default.
     if(count($result) == 0)
-      $result = [$this->term_name_and_id['park']];
+      echo "Missing term: ".$value;
     return $result;
-  }
-
-  protected function processTermName($term_name) {
-    return strtolower(str_replace(["-", " ", "(", ")"], "", $term_name));
   }
 
   /**
@@ -54,8 +58,9 @@ class MigrateParkLocationType extends ProcessPluginBase {
     if($this->term_loaded) return;
     $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vocabulary_name);
     foreach ($terms as $term) {
-      $this->term_name_and_id[$this->processTermName($term->name)] = $term->tid;
+      $this->term_name_and_id[$term->name] = $term->tid;
     }
+
     $this->term_loaded = TRUE;
   }
 }
