0cc951f4439f14a1142b213b866340b556a0a8c8
kxwang <xinju.wang@portlandoregon.gov>
2020-02-06 14:50:29 -0800

    POWR-1852 Fix demo search core

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -117,6 +117,11 @@ switch ($env) {
     break;
 }
 
+// Set the core to "Demo" on the Demo multidev
+if($env == 'powr-1852' || $env == 'demo') {
+  $config['search_api.server.searchstax']['backend_config']['connector_config']['core'] = 'Demo-1852';
+}
+
 /**
  * Overwrite Google Analytics tracking code in 'live' production site.
  *
