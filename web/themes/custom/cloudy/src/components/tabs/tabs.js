c871dedce2ec9eb95f270f5ae95cc3a0cc9834a0
Greg <greg@tikikon.com>
2020-03-16 13:59:09 -0700

    POWR-2002 updated component js from working static test file

--- web/themes/custom/cloudy/src/components/tabs/tabs.js
+++ web/themes/custom/cloudy/src/components/tabs/tabs.js
@@ -7,7 +7,7 @@ Drupal.behaviors.tab_handler = {
     var selectedTabId = 0;
     var selectedTab;
     var focusedTab;
-
+  
     // on initial load, check for tab navigation fragment in URL and activate indicated tab
     // hash no longer starts with #pane-
     if (urlHash.indexOf('#') == 0) {
@@ -16,13 +16,13 @@ Drupal.behaviors.tab_handler = {
       selectedTab.tab('show');
       selectTab(urlHash, selectedTab);
     }
-
+  
     $('#serviceModes a.nav-link').click(function(event) {
       event.preventDefault();
       // activate clicked tab using the link href
       selectTab($(this).attr('href'), $(this));
     });
-
+  
     $('#serviceModes').keydown(function(event) {
       if (event.which == 39) {
         // focus tab to the right, if it exists
@@ -46,14 +46,14 @@ Drupal.behaviors.tab_handler = {
         }
       }
     });
-
+  
     function getSelectedTabIndex(key) {
       // search key for hyphen, and use rest of string as index.
       // we are assuming key will always be in format #tab-1 or #pane-1
       var idx = key.indexOf('-');
       return parseInt(key.substring(idx + 1, key.length));
     }
-
+  
     function selectTab(linkHash, tab) {
       // add fragment to url; use replaceState to avoid filling up the history with tab changes;
       // we are not supporting browser navigation between tabs/url fragments at this time
@@ -62,16 +62,24 @@ Drupal.behaviors.tab_handler = {
       } else {
         location.hash = linkHash;
       }
-
+  
       // toggle tabindex; should be -1 for all non-selected tabs for accessibility purposes
       $('#serviceModes a.nav-link').attr('tabindex', -1);
       tab.removeAttr('tabindex');
-
+  
       // aria-hidden should be true for all but visible pane; bootstrap doesn't handle this
       $('#serviceModesContent .tab-pane').attr('aria-hidden', 'true');
-      var panel = $("#pane-" + linkHash.substr(1));
+      var panel = $("#" + linkHash.substr(1));
       panel.attr('aria-hidden', false);
-
+  
+      // manually hide/show panes; simply changing focus doesn't work in legacy Edge
+      $('.tab-pane').hide();
+      $('.tab-pane').removeClass('show');
+      panel.show();
+      panel.addClass('show');
+      $('#serviceModes a.nav-link').removeClass('active');
+      tab.addClass('active');
+  
       // focus the active tab; necessary in the case of page reload or direct navigation
       tab.focus();
       focusedTab = tab;
