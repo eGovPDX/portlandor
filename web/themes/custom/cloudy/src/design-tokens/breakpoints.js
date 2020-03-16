79744760bed738930afc683a626371842d256c0c
brittanysmart <brittany.smart@portlandoregon.gov>
2020-03-16 11:57:50 -0700

    POWR-2046 Add typography and breakpoint design tokens

---
+++ web/themes/custom/cloudy/src/design-tokens/breakpoints.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // QUESTION: Where should we set our grid-breakpoint map using these tokens?
+
+  // Copied from Bootstrap's breakpoint values
+  "breakpoint": {
+    "xs": { "value": "0" },
+    "sm": { "value": "576px" },
+    "md": { "value": "768px" },
+    "lg": { "value": "992px" },
+    "xl": { "value": "1200px" }
+  },
+}
--- web/themes/custom/cloudy/src/design-tokens/typography.js
+++ web/themes/custom/cloudy/src/design-tokens/typography.js
@@ -1,15 +1,5 @@
 module.exports = {
-  // QUESTION: How do we want to handle font wights?
-  // QUESTION: Where should we set our grid-breakpoint map using these tokens?
-
-  // Copied over Bootstrap's breakpoint values
-  "breakpoint": {
-    "xs": { "value": "0" },
-    "sm": { "value": "576px" },
-    "md": { "value": "768px" },
-    "lg": { "value": "992px" },
-    "xl": { "value": "1200px" }
-  },
+  // QUESTION: How do we want to handle font weight?
   "headerFontFamily": {
     "value": "'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif"
   },
