496861aa399cb08a2da27e68d27c6650f2951a31
Greg <greg@tikikon.com>
2020-03-16 12:33:39 -0700

    POWR-2002 updates to static test js, manually manipulate tabs and panes

--- web/test/Register your business _ Portland.gov_files/tabs.js
+++ web/test/Register your business _ Portland.gov_files/tabs.js
@@ -70,7 +70,11 @@ $(function() {
 
     // manually hide/show panes; simply changing focus doesn't work in legacy Edge
     $('.tab-pane').hide();
+    $('.tab-pane').removeClass('show');
     panel.show();
+    panel.addClass('show');
+    $('#serviceModes a.nav-link').removeClass('active');
+    tab.addClass('active');
 
     // focus the active tab; necessary in the case of page reload or direct navigation
     tab.focus();
