40815f7430bae01ce49f1a120a0689bd40829370
brittanysmart <brittany.smart@portlandoregon.gov>
2020-03-17 12:30:10 -0700

    POWR-1992 Update color design token naming

--- web/themes/custom/cloudy/src/design-tokens/color.js
+++ web/themes/custom/cloudy/src/design-tokens/color.js
@@ -1,58 +1,56 @@
 module.exports = {
   color: {
-    bg: {
-      neutral: {
-        '900': { value: '#00050A' }, // text
-        '500': { value: '#667E99' }, // text
-        '300': { value: '#AFBECF' },
-        '100': { value: '#EEF2F6' },
-        '0': {
-          value: '#FFFFFF',
-          comment: 'White',
-        }, // text
-      },
-      blue: {
-        '800': { value: '#174069' },// text
-        '500': { value: '#1F78D1' },// text
-        '300': { value: '#8CB3D9' },
-        '100': { value: '#E9F2FC' },
-      },
-      green: {
-        '800': { value: '#206035' },
-        '500': { value: '#339933' },// text
-        '300': { value: '#94D194' },
-        '100': { value: '#EFF5EF' },
-      },
-      yellow: {
-        '800': { value: '#B38600' },// text
-        '500': { value: '#F2B90D' },
-        '300': { value: '#FCDE83' },
-        '100': { value: '#FEF9E7' },
-      },
-      orange: {
-        '800': { value: '#793606' },
-        '500': { value: '#E66F1A' },// text
-        '300': { value: '#ECA979' },
-        '100': { value: '#F9F1EC' },
-      },
-      red: {
-        '800': { value: '#66191A' },
-        '500': { value: '#CC3333' },// text
-        '300': { value: '#CC6677' },
-        '100': { value: '#F9ECEC' },
-      },
-      purple: {
-        '800': { value: '#202060' },
-        '500': { value: '#4040BF' },// text
-        '300': { value: '#8C8CD9' },
-        '100': { value: '#EFEFF5' },
-      },
-      "teal": {
-        '800': { value: '#206060' },
-        '500': { value: '#22A0A0' },// text
-        '300': { value: '#8CD9D9' },
-        '100': { value: '#EDF7F7' },
-      },
+    neutral: {
+      '900': { value: '#00050A' }, // text
+      '500': { value: '#667E99' }, // text
+      '300': { value: '#AFBECF' },
+      '100': { value: '#EEF2F6' },
+      '0': {
+        value: '#FFFFFF',
+        comment: 'White',
+      }, // text
+    },
+    blue: {
+      '800': { value: '#174069' },// text
+      '500': { value: '#1F78D1' },// text
+      '300': { value: '#8CB3D9' },
+      '100': { value: '#E9F2FC' },
+    },
+    green: {
+      '800': { value: '#206035' },
+      '500': { value: '#339933' },// text
+      '300': { value: '#94D194' },
+      '100': { value: '#EFF5EF' },
+    },
+    yellow: {
+      '800': { value: '#B38600' },// text
+      '500': { value: '#F2B90D' },
+      '300': { value: '#FCDE83' },
+      '100': { value: '#FEF9E7' },
+    },
+    orange: {
+      '800': { value: '#793606' },
+      '500': { value: '#E66F1A' },// text
+      '300': { value: '#ECA979' },
+      '100': { value: '#F9F1EC' },
+    },
+    red: {
+      '800': { value: '#66191A' },
+      '500': { value: '#CC3333' },// text
+      '300': { value: '#CC6677' },
+      '100': { value: '#F9ECEC' },
+    },
+    purple: {
+      '800': { value: '#202060' },
+      '500': { value: '#4040BF' },// text
+      '300': { value: '#8C8CD9' },
+      '100': { value: '#EFEFF5' },
+    },
+    "teal": {
+      '800': { value: '#206060' },
+      '500': { value: '#22A0A0' },// text
+      '300': { value: '#8CD9D9' },
+      '100': { value: '#EDF7F7' },
     },
     // text: {
     //   black: { value: '{color.bg.neutral.900.value}' },
