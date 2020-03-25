1052bf63fd6ae88e9c35a8fad140629e4efac109
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-03-25 14:05:38 -0700

    POWR-2013 incrememtal commit of custom block, funcional but not complete

---
+++ web/modules/custom/portland/src/Plugin/Block/LegacyPathsBlock.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ * @file
+ * Contains \portland\Plugin\Block\SearchBlock
+ */
+
+namespace Drupal\portland\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
+
+/**
+ * Provides a Portland legacy paths block.
+ *
+ * @Block(
+ *   id = "portland_legacy_paths_block",
+ *   admin_label = @Translation("Portland Legacy Paths Block"),
+ *
+ * )
+ */
+class LegacyPathsBlock extends BlockBase {
+    private static $pog_base_url = "https://www.portlandoregon.gov/";
+    private static $help_text = "This content replaces the following pages on the old portlandoregon.gov website:";
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCacheMaxAge() {
+      return 0;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build() {
+      $current_path = \Drupal::service('path.current')->getPath();
+      $node = \Drupal::routeMatch()->getParameter('node');
+
+      if (isset($node)) {
+        $nid = $node->Id();
+        $type = $node->getEntityTypeId();
+        if ($nid && $type) {
+          $redirects = \Drupal::service('redirect.repository')->findByDestinationUri(["internal:/$type/$nid", "entity:$type/$nid"]);
+        }
+        $legacy_paths = [];
+        foreach ($redirects as $redirect) {
+          $legacy_paths[] = $redirect->getSource()['path'];
+        }
+
+        $render_array = [
+          '#theme' => 'portland_legacy_paths_block',
+          '#pog_base_url' => self::$pog_base_url,
+          '#legacy_paths' => $legacy_paths,
+          '#help_text' => t(self::$help_text),
+        ];
+
+        return $render_array;
+      }
+
+      return [
+        '#theme' => 'portland_legacy_paths_block',
+      ];
+    }
+
+    public static function buildRenderArray($node_latest_revision, $node_current_revision, $node_default_revision) {
+      if($node_current_revision == NULL || $node_latest_revision == NULL || $node_default_revision == NULL) return;
+
+      $nid = $node_current_revision->nid->value;
+      $node_latest_vid = $node_latest_revision->vid->value;
+      $node_latest_status = $node_latest_revision->status->value;
+      $node_current_vid = $node_current_revision->vid->value;
+      $node_current_status = $node_current_revision->status->value;
+      $node_current_is_archived = ($node_current_revision->moderation_state->value == 'archived');
+      $node_default_vid = $node_default_revision->vid->value;
+      $node_default_status = $node_default_revision->status->value;
+
+      // Set the default
+      $render_array = [
+        '#theme' => 'portland_legacy_paths_block',
+        '#alert_color' => self::getSeverity($node_current_revision),
+        '#alert_icon' => self::$severity_icon[self::getSeverity($node_current_revision)],
+        '#current_revision_state' => self::getModerationStateInString($node_current_revision),
+        '#latest_revision_state' => [
+          '#markup' => ' and new <strong>'. self::getModerationStateInString($node_latest_revision) . '</strong> revision exists.'],
+        '#revision_link' => "/node/$nid/revisions/$node_latest_vid/view",
+        '#revision_link_text' => 'View latest version',
+      ];
+      /*
+      latest == current > default
+      default is published
+        (current is archived) ? DANGER : WARN, current state, NULL, view published
+      default is archived
+        AS_IS, current state, NULL, NULL
+      */
+      if($node_latest_vid == $node_current_vid && $node_current_vid > $node_default_vid) {
+        if($node_default_status == 1) {
+          $render_array['#alert_color'] = ($node_current_is_archived) ? 'danger' : 'warning';
+          $render_array['#alert_icon'] = ($node_current_is_archived) ? 'ban' : 'exclamation-circle';
+          $render_array['#latest_revision_state'] = NULL;
+          $render_array['#revision_link'] = "/node/$nid";
+          $render_array['#revision_link_text'] = "View published version";
+        }
+        else {
+          $render_array['#latest_revision_state'] = NULL;
+          $render_array['#revision_link'] = NULL;
+          $render_array['#revision_link_text'] = NULL;
+        }
+      }
+      /*
+      latest > current >= default
+        (current is archived) ? DANGER : WARN, current state, latest state, view latest
+      */
+      else if($node_latest_vid > $node_current_vid && $node_current_vid >= $node_default_vid) {
+        $render_array['#alert_color'] = ($node_current_is_archived) ? 'danger' : 'warning';
+        $render_array['#alert_icon'] = ($node_current_is_archived) ? 'ban' : 'exclamation-circle';
+      }
+      /*
+      latest = current = default
+        default is published
+          INFO, current state
+        default is archived
+          DANGER, current state
+      */
+      else if($node_latest_vid == $node_current_vid && $node_current_vid == $node_default_vid) {
+        $render_array['#latest_revision_state'] = NULL;
+        $render_array['#revision_link'] = NULL;
+        $render_array['#revision_link_text'] = NULL;
+      }
+      /*
+      latest = default > current
+        default is published
+          (current is archived) ? DANGER : WARN, current state, latest state, view published
+        default is archived
+          (current is archived) ? DANGER : WARN, current state, latest state, view latest
+      */
+      else if($node_latest_vid == $node_default_vid && $node_default_vid > $node_current_vid ) {
+        $render_array['#alert_color'] = ($node_current_is_archived) ? 'danger' : 'warning';
+        $render_array['#alert_icon'] = ($node_current_is_archived) ? 'ban' : 'exclamation-circle';
+        if($node_default_status == 1) {
+          $render_array['#revision_link'] = "/node/$nid";
+          $render_array['#revision_link_text'] = "View published version";
+        }
+      }
+      /*
+      latest > default > current
+        (current is archived) ? DANGER : WARN, current state, latest state, view latest
+      */
+      else if($node_latest_vid > $node_default_vid && $node_default_vid > $node_current_vid ) {
+        $render_array['#alert_color'] = ($node_current_is_archived) ? 'danger' : 'warning';
+        $render_array['#alert_icon'] = ($node_current_is_archived) ? 'ban' : 'exclamation-circle';
+      }
+
+      return $render_array;
+    }
+
+    public static function getSeverity($node) {
+      if($node->status->value == 0) {
+        return ($node->moderation_state->value == 'archived') ? 'danger' : 'warning';
+      }
+      return 'success';
+    }
+
+    public static function getModerationStateInString($node) {
+      $published = $node->status->value;
+      $moderation_state = $node->moderation_state->value;
+      if($published == "0") {
+        switch($moderation_state) {
+          case "archived":
+            $result = "Unpublished/archived";
+            break;
+          case "review":
+            $result = "Unpublished and in review";
+            break;
+          default:
+            $result = "Unpublished draft";
+        }
+      }
+      else {
+        $result = "Published";
+      }
+      return $result;
+    }
+
+    public static function loadLatestRevision($nid) {
+      // Load the latest revision
+      $latestNode = \Drupal::entityTypeManager()->getStorage('node')->getQuery()
+                  ->latestRevision()
+                  ->condition('nid', $nid)
+                  ->accessCheck(FALSE)
+                  ->execute();
+      reset($latestNode);
+      $latestRevisionId = key($latestNode);
+      return \Drupal::entityTypeManager()->getStorage('node')->loadRevision($latestRevisionId);
+    }
+  }
