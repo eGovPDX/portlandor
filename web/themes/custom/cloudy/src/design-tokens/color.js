7686c0ac64fd95de207eb1fd3897f84a8a8cd481
brittanysmart <brittany.smart@portlandoregon.gov>
2020-03-26 16:43:54 -0700

    POWR-1992 Update color values to pass AA rating

--- web/themes/custom/cloudy/src/design-tokens/color.js
+++ web/themes/custom/cloudy/src/design-tokens/color.js
@@ -12,32 +12,40 @@ module.exports = {
     },
     blue: {
       '800': { value: '#174069' },// text
-      '500': { value: '#1F78D1' },// text
+      // '500': { value: '#1F78D1' },// ref to old value - updated to meet AA at 14px. Will remove later.
+      '500': { value: '#1E73C8' },// text
       '300': { value: '#8CB3D9' },
       '100': { value: '#E9F2FC' },
     },
     green: {
       '800': { value: '#206035' },
-      '500': { value: '#339933' },// text
+      // '500': { value: '#339933' },// ref to old value - updated to meet AA at 14px. Will remove later.
+      '500': { value: '#39862D' },// text
       '300': { value: '#94D194' },
       '100': { value: '#EFF5EF' },
     },
     yellow: {
-      '800': { value: '#B38600' },// text
+      // '800': { value: '#B38600' },// ref to old value - updated to meet AA at 14px. Will remove later.
+      '800': { value: '#866B09' },// text
       '500': { value: '#F2B90D' },
       '300': { value: '#FCDE83' },
-      '100': { value: '#FEF9E7' },
+      // '100': { value: '#FEF9E7' }, // ref to old value - updated to meet AA at 14px. Will remove later.
+      '100': { value: '#F7F5EE' },
     },
     orange: {
       '800': { value: '#793606' },
-      '500': { value: '#E66F1A' },// text
+      // '500': { value: '#E66F1A' },// ref to old value - updated to new orange to meet AA at 14px. Will remove later.
+      '500': { value: '#B85814' },// text
       '300': { value: '#ECA979' },
       '100': { value: '#F9F1EC' },
     },
     red: {
-      '800': { value: '#66191A' },
-      '500': { value: '#CC3333' },// text
-      '300': { value: '#CC6677' },
+      // '800': { value: '#66191A' }, // ref to old value - updated to new orange to meet AA at 14px. Will remove later.
+      '800': { value: '#602020' },
+      // '500': { value: '#CC3333' },// ref to old value - updated to new orange to meet AA at 14px. Will remove later.
+      '500': { value: '#BF4040' },// text
+      // '300': { value: '#CC6677' },
+      '300': { value: '#D98C8C' }, // ref to old value - updated to new orange to meet AA at 14px. Will remove later.
       '100': { value: '#F9ECEC' },
     },
     purple: {
@@ -46,25 +54,11 @@ module.exports = {
       '300': { value: '#8C8CD9' },
       '100': { value: '#EFEFF5' },
     },
-    "teal": {
+    teal: {
       '800': { value: '#206060' },
       '500': { value: '#22A0A0' },// text
       '300': { value: '#8CD9D9' },
       '100': { value: '#EDF7F7' },
-    },
-    // text: {
-    //   black: { value: '{color.bg.neutral.900.value}' },
-    //   white: { value: '{color.bg.neutral.0.value}' },
-    //   gray: {},
-    //   "blue": {
-    //     "value": "{ color.bg}"
-    //   }
-    //   green:
-    //     yellow:
-    //   orange:
-    //     red:
-    //   purple:
-    //     teal:
-    // },
-  },
+    }
+  }
 };
