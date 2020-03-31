8e3c839144966edfcde35686feb1eebebfd3782f
brittanysmart <brittany.smart@portlandoregon.gov>
2020-03-31 13:24:07 -0700

    POWR-1992 Refine color palette, update tile, hover

--- web/themes/custom/cloudy/src/design-tokens/color.js
+++ web/themes/custom/cloudy/src/design-tokens/color.js
@@ -1,51 +1,44 @@
 module.exports = {
   color: {
     neutral: {
-      '900': { value: '#00050A' }, // text
-      '500': { value: '#667E99' }, // text
-      '300': { value: '#AFBECF' },
-      '100': { value: '#EEF2F6' },
+      '900': { value: '#040506' }, // text
+      '700': { value: '#434F60' }, // text
+      '500': { value: '#74839A' }, // text
+      '300': { value: '#B3BDCC' },
+      '100': { value: '#EFF1F5' },
       '0': {
         value: '#FFFFFF',
         comment: 'White',
       }, // text
     },
     blue: {
-      '800': { value: '#174069' },// text
-      // '500': { value: '#1F78D1' },// ref to old value - updated to meet AA at 14px. Will remove later.
-      '500': { value: '#1E73C8' },// text
-      '300': { value: '#8CB3D9' },
-      '100': { value: '#E9F2FC' },
+      '800': { value: '#234476' },// text
+      '500': { value: '#1D5DBF' },// text
+      '300': { value: '#A3BADB' },
+      '100': { value: '#ECF1F9' },
     },
     green: {
       '800': { value: '#206035' },
-      // '500': { value: '#339933' },// ref to old value - updated to meet AA at 14px. Will remove later.
       '500': { value: '#39862D' },// text
       '300': { value: '#94D194' },
       '100': { value: '#EFF5EF' },
     },
     yellow: {
-      // '800': { value: '#B38600' },// ref to old value - updated to meet AA at 14px. Will remove later.
       '800': { value: '#866B09' },// text
       '500': { value: '#F2B90D' },
       '300': { value: '#FCDE83' },
-      // '100': { value: '#FEF9E7' }, // ref to old value - updated to meet AA at 14px. Will remove later.
       '100': { value: '#F7F5EE' },
     },
     orange: {
       '800': { value: '#793606' },
-      // '500': { value: '#E66F1A' },// ref to old value - updated to new orange to meet AA at 14px. Will remove later.
       '500': { value: '#B85814' },// text
       '300': { value: '#ECA979' },
       '100': { value: '#F9F1EC' },
     },
     red: {
-      // '800': { value: '#66191A' }, // ref to old value - updated to new orange to meet AA at 14px. Will remove later.
       '800': { value: '#602020' },
-      // '500': { value: '#CC3333' },// ref to old value - updated to new orange to meet AA at 14px. Will remove later.
       '500': { value: '#BF4040' },// text
-      // '300': { value: '#CC6677' },
-      '300': { value: '#D98C8C' }, // ref to old value - updated to new orange to meet AA at 14px. Will remove later.
+      '300': { value: '#D98C8C' },
       '100': { value: '#F9ECEC' },
     },
     purple: {
