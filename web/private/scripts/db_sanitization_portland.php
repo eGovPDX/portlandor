e577de9f874a80afa283436d53b0ea77f7e221af
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-04-09 14:58:00 -0700

    POWR-2001 incremental checkin to switch to a more urgent story

--- web/private/scripts/db_sanitization_portland.php
+++ web/private/scripts/db_sanitization_portland.php
@@ -3,7 +3,7 @@
 // destroy the canonical version of the data.
 if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
 
-	$sanitize_query = "update users_field_data inner join user__roles on uid = entity_id set pass = '', mail = concat('user+', uid, '@localhost.localdomain'), init = concat('user+', uid, '@localhost.localdomain') where roles_target_id <> 'administrator';";
+	$sanitize_query = "update users_field_data left outer join user__roles on uid = entity_id set pass = '', mail = concat('user+', uid, '@localhost.localdomain'), init = concat('user+', uid, '@localhost.localdomain') where roles_target_id IS NULL or roles_target_id <> 'administrator';";
 	$webform_query = "truncate table webform_submission; truncate table webform_submission_data; truncate table webform_submission_log;";
 	$cache_query = "truncate table cache_bootstrap; truncate table cache_config; truncate table cache_container; truncate table cache_data; truncate table cache_default; truncate table cache_discovery; truncate table cache_entity; truncate table cache_page; truncate table cache_render;";
 
