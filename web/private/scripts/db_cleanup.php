5487997940d0f4ce018b926fda030ae2d616c0ad
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-04-13 10:55:31 -0700

    POWR-2001 truncate watchdog table

--- web/private/scripts/db_cleanup.php
+++ web/private/scripts/db_cleanup.php
@@ -4,10 +4,16 @@
 if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
 
 	$webform_query = "truncate table webform_submission; truncate table webform_submission_data; truncate table webform_submission_log;";
+	$watchdog_query = "truncate table watchdog;";
 
 	// Delete webform submission data
 	echo "Deleting webform submissions...\n";
 	passthru('drush sql-query "' . $webform_query . '"');
 	echo "Webform submission deletion complete.\n";
+
+	// Delete log data
+	echo "Deleting log entries...\n";
+	passthru('drush sql-query "' . $watchdog_query . '"');
+	echo "Log entry deletion complete.\n";
 
 }
