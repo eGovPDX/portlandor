e5f3312348747b5c74c0360e4b9110d9ce5329ec
brittanysmart <brittany.smart@portlandoregon.gov>
2020-04-14 15:32:03 -0700

    POWR-2048 Refine typography

--- web/themes/custom/cloudy/src/design-tokens/typography.js
+++ web/themes/custom/cloudy/src/design-tokens/typography.js
@@ -12,24 +12,24 @@ module.exports = {
       "fontSize": { "value": "28px" },
       "lineHeight": { "value": "34px" },
       "md": {
-        "fontSize": { "value": "48px" },
-        "lineHeight": { "value": "56px" }
+        "fontSize": { "value": "40px" },
+        "lineHeight": { "value": "48px" }
       }
     },
     "heading-02": {
       "fontSize": { "value": "24px" },
       "lineHeight": { "value": "30px" },
       "md": {
-        "fontSize": { "value": "40px" },
-        "lineHeight": { "value": "48px" }
+        "fontSize": { "value": "32px" },
+        "lineHeight": { "value": "38px" }
       }
     },
     "heading-03": {
       "fontSize": { "value": "22px" },
       "lineHeight": { "value": "28px" },
       "md": {
-        "fontSize": { "value": "32px" },
-        "lineHeight": { "value": "40px" }
+        "fontSize": { "value": "28px" },
+        "lineHeight": { "value": "36px" }
       }
     },
     "heading-04": {
@@ -54,18 +54,18 @@ module.exports = {
     },
     "body": {
       "fontSize": { "value": "{ type.base.fontSize.value }" },
-      "lineHeight": { "value": "20px" },
+      "lineHeight": { "value": "24px" },
       "md": {
-        "lineHeight": { "value": "24px" }
+        "lineHeight": { "value": "27px" }
       }
     }
   },
   "lead": {
     "fontSize": { "value": "18px" },
-    "lineHeight": { "value": "24px" },
+    "lineHeight": { "value": "26px" },
     "md": {
       "fontSize": { "value": "20px" },
-      "lineHeight": { "value": "28px" }
+      "lineHeight": { "value": "30px" }
     }
   },
   "subtitle": {
