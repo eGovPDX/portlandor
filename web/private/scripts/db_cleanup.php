854758176e882ce78882f378b140cd6aaf993c09
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-04-15 12:29:00 -0700

    POWR-2001 updated delete node revisions query, housekeeping

--- web/private/scripts/db_cleanup.php
+++ web/private/scripts/db_cleanup.php
@@ -16,4 +16,69 @@ if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
 	passthru('drush sql-query "' . $watchdog_query . '"');
 	echo "Log entry deletion complete.\n";
 
+	// Delete node revisions older than 30 days
+	// node revision delete
+	$node_revision_query = "		drop temporary table if exists tmp_old_node_revs;
+		create temporary table tmp_old_node_revs
+		select vid, date_format(from_unixtime(revision_timestamp), '%e %b %Y') AS 'date', revision_timestamp
+		from node_revision
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) 
+		order by revision_timestamp;
+
+		delete C
+		from node_revision__field_body_content as C
+		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+
+		delete C
+		from node_revision__field_summary as C
+		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+
+		delete C
+		from node_revision__field_topics as C
+		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+
+		delete C
+		from node_revision__field_display_groups as C
+		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+
+		delete C
+		from node_revision__field_service_mode as C
+		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+
+		delete C
+		from node_revision__field_audience as C
+		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+
+		delete C
+		from node_revision__field_related_content as C
+		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+
+		delete C
+		from node_revision__field_popular as C
+		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+
+		delete C
+		from node_revision__field_page_type as C
+		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+
+		drop temporary table tmp_old_node_revs;
+
+		-- now delete the revisions themselves
+		delete from node_revision
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));";
+
+	// Delete log data
+	echo "Deleting node revisions older than 30 days...\n";
+	passthru('drush sql-query "' . $watchdog_query . '"');
+	echo "Node revision deletion complete.\n";
+
 }
