66b07d6c9c5c8da50a6192d819eab4d79214b8cf
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-04-17 11:14:08 -0700

    POWR-2001 incremental commit so I can work on something else

--- web/private/scripts/db_cleanup.php
+++ web/private/scripts/db_cleanup.php
@@ -22,53 +22,53 @@ if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
 		create temporary table tmp_old_node_revs
 		select vid, date_format(from_unixtime(revision_timestamp), '%e %b %Y') AS 'date', revision_timestamp
 		from node_revision
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) 
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day))
 		order by revision_timestamp;
 
 		delete C
 		from node_revision__field_body_content as C
 		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
 
 		delete C
 		from node_revision__field_summary as C
 		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
 
 		delete C
 		from node_revision__field_topics as C
 		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
 
 		delete C
 		from node_revision__field_display_groups as C
 		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
 
 		delete C
 		from node_revision__field_service_mode as C
 		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
 
 		delete C
 		from node_revision__field_audience as C
 		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
 
 		delete C
 		from node_revision__field_related_content as C
 		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
 
 		delete C
 		from node_revision__field_popular as C
 		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
 
 		delete C
 		from node_revision__field_page_type as C
 		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));
+		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
 
 		drop temporary table tmp_old_node_revs;
 
