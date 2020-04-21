bcf4db6fb54e2c8ff245bfc4e4ff219a3e311941
Julie Hernandez <julegan@users.noreply.github.com>
2020-04-20 17:35:04 -0700

    POWR-2048 final pass on typography

--- web/themes/custom/cloudy/src/design-tokens/typography.js
+++ web/themes/custom/cloudy/src/design-tokens/typography.js
@@ -12,86 +12,82 @@ module.exports = {
       "fontSize": { "value": "28px" },
       "lineHeight": { "value": "34px" },
       "md": {
-        "fontSize": { "value": "40px" },
-        "lineHeight": { "value": "48px" }
+        "fontSize": { "value": "32px" },
+        "lineHeight": { "value": "40px" },
       }
     },
     "heading-02": {
       "fontSize": { "value": "24px" },
       "lineHeight": { "value": "30px" },
       "md": {
-        "fontSize": { "value": "32px" },
-        "lineHeight": { "value": "38px" }
+        "fontSize": { "value": "28px" },
+        "lineHeight": { "value": "36px" },
       }
     },
     "heading-03": {
       "fontSize": { "value": "22px" },
       "lineHeight": { "value": "28px" },
       "md": {
-        "fontSize": { "value": "28px" },
-        "lineHeight": { "value": "36px" }
+        "fontSize": { "value": "24px" },
+        "lineHeight": { "value": "32px" },
       }
     },
     "heading-04": {
       "fontSize": { "value": "20px" },
       "lineHeight": { "value": "26px" },
       "md": {
-        "fontSize": { "value": "24px" },
-        "lineHeight": { "value": "32px" }
+        "fontSize": { "value": "20px" },
+        "lineHeight": { "value": "34px" },
       }
     },
     "heading-05": {
-      "fontSize": { "value": "18px" },
+      "fontSize": { "value": "16px" },
       "lineHeight": { "value": "24px" },
       "md": {
-        "fontSize": { "value": "20px" },
-        "lineHeight": { "value": "24px" }
+        "fontSize": { "value": "16px" },
+        "lineHeight": { "value": "24px" },
       }
     },
     "heading-06": {
-      "fontSize": { "value": "16px" },
-      "lineHeight": { "value": "24px" },
+      "fontSize": { "value": "14" },
+      "lineHeight": { "value": "20px" },
     },
     "body": {
       "fontSize": { "value": "{ type.base.fontSize.value }" },
       "lineHeight": { "value": "24px" },
       "md": {
-        "lineHeight": { "value": "27px" }
+        "lineHeight": { "value": "24px" },
       }
     }
   },
   "lead": {
     "fontSize": { "value": "18px" },
     "lineHeight": { "value": "26px" },
-    "md": {
-      "fontSize": { "value": "20px" },
-      "lineHeight": { "value": "30px" }
-    }
   },
   "subtitle": {
-    "fontSize": { "value": "16px" },
+    "fontSize": { "value": "{ type.body.fontSize.value }" },
     "lineHeight": { "value": "24px" },
   },
   "helper-text": {
     "fontSize": { "value": "14px" },
     "lineHeight": { "value": "16px" },
     "md": {
-      "lineHeight": { "value": "24px" }
+      "lineHeight": { "value": "24px" },
     }
   },
   "ui-text": {
     "fontSize": { "value": "{ type.body.fontSize.value }" },
-    "lineHeight": { "value": "22px" },
+    "lineHeight": { "value": "24px" },
   },
   "permalink-text": {
     "fontSize": { "value": "{ type.body.fontSize.value }" },
     "lineHeight": { "value": "24px" },
   },
   "time-text": {
-    "fontSize": { "value": "{ type.body.fontSize.value }" },
+    "fontSize": { "value": "18px" },
     "lineHeight": { "value": "24px" },
     "md": {
-      "fontSize": { "value": "20px" }
+      "fontSize": { "value": "20px" },
     }
   }
 }
