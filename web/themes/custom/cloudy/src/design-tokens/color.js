afafcdadd088b0fac17ccbef56a8e9585ebb9ee9
brittanysmart <brittany.smart@portlandoregon.gov>
2020-04-21 16:11:55 -0700

    POWR-2133 Move typography, add color util classes

--- web/themes/custom/cloudy/src/design-tokens/color.js
+++ web/themes/custom/cloudy/src/design-tokens/color.js
@@ -1,57 +1,57 @@
 module.exports = {
   color: {
     neutral: {
-      '900': { value: '#040506' }, // text
-      '800': { value: '#2A313C' }, // text
-      '700': { value: '#495669' }, // text
-      '500': { value: '#7A899F' }, // text
+      '900': { value: '#040506' },
+      '800': { value: '#2A313C' },
+      '700': { value: '#495669' },
+      '500': { value: '#7A899F' },
       '300': { value: '#B3BDCC' },
       '200': { value: '#D1D7E0' },
       '100': { value: '#EFF1F5' },
       '0': {
         value: '#FFFFFF',
         comment: 'White',
-      }, // text
+      },
     },
     blue: {
-      '800': { value: '#234476' },// text
-      '500': { value: '#1D5DBF' },// text
+      '800': { value: '#234476' },
+      '500': { value: '#1D5DBF' },
       '300': { value: '#A3BADB' },
       '100': { value: '#ECF1F9' },
     },
     green: {
       '800': { value: '#206035' },
-      '500': { value: '#39862D' },// text
+      '500': { value: '#39862D' },
       '300': { value: '#94D194' },
       '100': { value: '#EFF5EF' },
     },
     yellow: {
-      '800': { value: '#866B09' },// text
+      '800': { value: '#866B09' },
       '500': { value: '#F2B90D' },
       '300': { value: '#FCDE83' },
       '100': { value: '#F7F5EE' },
     },
     orange: {
       '800': { value: '#793606' },
-      '500': { value: '#B85814' },// text
+      '500': { value: '#B85814' },
       '300': { value: '#ECA979' },
       '100': { value: '#F9F1EC' },
     },
     red: {
       '800': { value: '#602020' },
-      '500': { value: '#BF4040' },// text
+      '500': { value: '#BF4040' },
       '300': { value: '#D98C8C' },
       '100': { value: '#F9ECEC' },
     },
     purple: {
       '800': { value: '#202060' },
-      '500': { value: '#4040BF' },// text
+      '500': { value: '#4040BF' },
       '300': { value: '#8C8CD9' },
       '100': { value: '#EFEFF5' },
     },
     teal: {
       '800': { value: '#205860' },
-      '500': { value: '#077D8D' },// text
+      '500': { value: '#077D8D' },
       '300': { value: '#8CD0D9' },
       '100': { value: '#EDF6F7' },
     }
