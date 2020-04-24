73161d5933a32c00e2d5086fc14c519a47b2d021
brittanysmart <brittany.smart@portlandoregon.gov>
2020-04-24 16:31:40 -0700

    POWR-2048 Adjust typography to inherit from Bootstrap variables

--- web/themes/custom/cloudy/src/design-tokens/typography.js
+++ web/themes/custom/cloudy/src/design-tokens/typography.js
@@ -6,66 +6,18 @@ module.exports = {
   },
   "type": {
     "base": {
-      "fontSize": { "value": "16px" }
-    },
-    "heading-01": {
-      "fontSize": { "value": "28px" },
-      "lineHeight": { "value": "34px" },
-      "md": {
-        "fontSize": { "value": "32px" },
-        "lineHeight": { "value": "40px" },
-      }
-    },
-    "heading-02": {
-      "fontSize": { "value": "24px" },
-      "lineHeight": { "value": "30px" },
-      "md": {
-        "fontSize": { "value": "28px" },
-        "lineHeight": { "value": "36px" },
-      }
-    },
-    "heading-03": {
-      "fontSize": { "value": "22px" },
-      "lineHeight": { "value": "28px" },
-      "md": {
-        "fontSize": { "value": "24px" },
-        "lineHeight": { "value": "32px" },
-      }
-    },
-    "heading-04": {
-      "fontSize": { "value": "20px" },
-      "lineHeight": { "value": "26px" },
-      "md": {
-        "fontSize": { "value": "20px" },
-        "lineHeight": { "value": "34px" },
-      }
-    },
-    "heading-05": {
       "fontSize": { "value": "16px" },
-      "lineHeight": { "value": "24px" },
       "md": {
-        "fontSize": { "value": "16px" },
-        "lineHeight": { "value": "24px" },
+        "fontSize": { "value": "18px" },
       }
     },
-    "heading-06": {
-      "fontSize": { "value": "14" },
-      "lineHeight": { "value": "20px" },
-    },
-    "body": {
-      "fontSize": { "value": "{ type.base.fontSize.value }" },
-      "lineHeight": { "value": "24px" },
-      "md": {
-        "lineHeight": { "value": "24px" },
-      }
-    }
   },
   "lead": {
     "fontSize": { "value": "18px" },
     "lineHeight": { "value": "26px" },
   },
   "subtitle": {
-    "fontSize": { "value": "{ type.body.fontSize.value }" },
+    "fontSize": { "value": "{ type.base.fontSize.value }" },
     "lineHeight": { "value": "24px" },
   },
   "helper-text": {
@@ -73,11 +25,11 @@ module.exports = {
     "lineHeight": { "value": "24px" },
   },
   "ui-text": {
-    "fontSize": { "value": "{ type.body.fontSize.value }" },
+    "fontSize": { "value": "{ type.base.fontSize.value }" },
     "lineHeight": { "value": "24px" },
   },
   "permalink-text": {
-    "fontSize": { "value": "{ type.body.fontSize.value }" },
+    "fontSize": { "value": "{ type.base.fontSize.value }" },
     "lineHeight": { "value": "24px" },
   },
   "time-text": {
