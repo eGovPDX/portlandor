5c83d8c75e52be63d3453a50503076f9dbfaef7c
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-04-27 16:01:40 -0700

    POWR-2001 removed log deletion, updated revision deletion to not delete current revision and break

--- web/private/db_cleanup.php
+++ web/private/db_cleanup.php
@@ -3,28 +3,72 @@
 // destroy the canonical version of the data.
 if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
 
+
+	// Delete webform submission data
 	$webform_query = "truncate table webform_submission; truncate table webform_submission_data; truncate table webform_submission_log;";
-	$watchdog_query = "truncate table watchdog;";
-
-	// // Delete webform submission data
-	// echo "Deleting webform submissions...\n";
-	// passthru('drush sql-query "' . $webform_query . '"');
-	// echo "Webform submission deletion complete.\n";
-
-	// // Delete log data
-	// echo "Deleting log entries...\n";
-	// passthru('drush sql-query "' . $watchdog_query . '"');
-	// echo "Log entry deletion complete.\n";
+	echo "Deleting webform submissions...\n";
+	passthru('drush sql-query "' . $webform_query . '"');
+	echo "Webform submission deletion complete.\n";
 
 	// Delete node revisions older than 30 days
+	$revisions_query = "
+		DROP TEMPORARY TABLE IF EXISTS expired_revisions;
+		CREATE TEMPORARY TABLE expired_revisions
+		SELECT t.vid AS vid
+		FROM
+			(SELECT r.vid AS vid, r.revision_timestamp AS revision_timestamp
+			FROM
+			node_field_data n
+				INNER JOIN node_revision r ON r.nid = n.nid
+			WHERE (changed < (CURRENT_DATE - INTERVAL 30 DAY)) AND ((n.vid <> r.vid))
+			GROUP BY n.nid, r.vid, r.revision_timestamp
+			ORDER BY revision_timestamp DESC
+			LIMIT 9223372036854775807 OFFSET 0) t
+		WHERE revision_timestamp < (CURRENT_DATE - INTERVAL 30 DAY);
 
-	echo "Deleting node revisions older than 30 days...\n";
-	$vids = \Drupal::entityTypeManager()->getStorage('node')->revisionIds($node);
-	// If revision id is not default, remove it.
-  if ($vid !== $node->getLoadedRevisionId()) {
-    print 'Removing revision ' . $vid . PHP_EOL;
-    \Drupal::entityTypeManager()->getStorage('node')->deleteRevision($vid);
-  }
-	echo "Node revision deletion complete.\n";
+		DELETE C
+		FROM node_revision__field_body_content C
+		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+
+		DELETE C
+		FROM node_revision__field_summary C
+		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+
+		DELETE C
+		FROM node_revision__field_topics C
+		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+
+		DELETE C
+		FROM node_revision__field_display_groups C
+		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+
+		DELETE C
+		FROM node_revision__field_service_mode C
+		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+
+		DELETE C
+		FROM node_revision__field_audience C
+		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+
+		DELETE C
+		FROM node_revision__field_related_content C
+		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+
+		DELETE C
+		FROM node_revision__field_popular C
+		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+
+		DELETE C
+		FROM node_revision__field_page_type C
+		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+
+		DELETE R
+		FROM node_revision R
+		INNER JOIN expired_revisions REV ON R.vid = REV.vid;
+
+		DROP TEMPORARY TABLE IF EXISTS expired_revisions;";
+	echo "Deleting revision data older than 30 days...\n";
+	passthru('drush sql-query "' . $revisions_query . '"');
+	echo "Revision deletion complete.\n";
 
 }
