7d62c577be598d653bbe8c739b4fb9fc609541b7
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-04-27 17:04:12 -0700

    POWR-2001-1 removed query code indentation in string, was causing errors (not sure why)

--- web/private/db_cleanup.php
+++ web/private/db_cleanup.php
@@ -12,61 +12,61 @@ if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
 
 	// Delete node revisions older than 30 days
 	$revisions_query = "
-		DROP TEMPORARY TABLE IF EXISTS expired_revisions;
-		CREATE TEMPORARY TABLE expired_revisions
-		SELECT t.vid AS vid
-		FROM
-			(SELECT r.vid AS vid, r.revision_timestamp AS revision_timestamp
-			FROM
-			node_field_data n
-				INNER JOIN node_revision r ON r.nid = n.nid
-			WHERE (changed < (CURRENT_DATE - INTERVAL 30 DAY)) AND ((n.vid <> r.vid))
-			GROUP BY n.nid, r.vid, r.revision_timestamp
-			ORDER BY revision_timestamp DESC
-			LIMIT 9223372036854775807 OFFSET 0) t
-		WHERE revision_timestamp < (CURRENT_DATE - INTERVAL 30 DAY);
+DROP TEMPORARY TABLE IF EXISTS expired_revisions;
+CREATE TEMPORARY TABLE expired_revisions
+SELECT t.vid AS vid
+FROM
+(SELECT r.vid AS vid, r.revision_timestamp AS revision_timestamp
+FROM
+node_field_data n
+INNER JOIN node_revision r ON r.nid = n.nid
+WHERE (changed < (CURRENT_DATE - INTERVAL 30 DAY)) AND ((n.vid <> r.vid))
+GROUP BY n.nid, r.vid, r.revision_timestamp
+ORDER BY revision_timestamp DESC
+LIMIT 9223372036854775807 OFFSET 0) t
+WHERE revision_timestamp < (CURRENT_DATE - INTERVAL 30 DAY);
 
-		DELETE C
-		FROM node_revision__field_body_content C
-		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+DELETE C
+FROM node_revision__field_body_content C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		DELETE C
-		FROM node_revision__field_summary C
-		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+DELETE C
+FROM node_revision__field_summary C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		DELETE C
-		FROM node_revision__field_topics C
-		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+DELETE C
+FROM node_revision__field_topics C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		DELETE C
-		FROM node_revision__field_display_groups C
-		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+DELETE C
+FROM node_revision__field_display_groups C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		DELETE C
-		FROM node_revision__field_service_mode C
-		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+DELETE C
+FROM node_revision__field_service_mode C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		DELETE C
-		FROM node_revision__field_audience C
-		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+DELETE C
+FROM node_revision__field_audience C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		DELETE C
-		FROM node_revision__field_related_content C
-		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+DELETE C
+FROM node_revision__field_related_content C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		DELETE C
-		FROM node_revision__field_popular C
-		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+DELETE C
+FROM node_revision__field_popular C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		DELETE C
-		FROM node_revision__field_page_type C
-		INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
+DELETE C
+FROM node_revision__field_page_type C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		DELETE R
-		FROM node_revision R
-		INNER JOIN expired_revisions REV ON R.vid = REV.vid;
+DELETE R
+FROM node_revision R
+INNER JOIN expired_revisions REV ON R.vid = REV.vid;
 
-		DROP TEMPORARY TABLE IF EXISTS expired_revisions;";
+DROP TEMPORARY TABLE IF EXISTS expired_revisions;";
 	echo "Deleting revision data older than 30 days...\n";
 	passthru('drush sql-query "' . $revisions_query . '"');
 	echo "Revision deletion complete.\n";
