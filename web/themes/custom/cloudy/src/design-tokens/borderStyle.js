3e97b811802cb76ee0da54b2d3ae448d9f5fa84c
brittanysmart <brittany.smart@portlandoregon.gov>
2020-04-28 12:27:05 -0700

    POWR-2133 Add token values to utility classes

---
+++ web/themes/custom/cloudy/src/design-tokens/borderStyle.js
@@ -0,0 +1,8 @@
+module.exports = {
+  borderStyle: {
+    'solid': { value: 'solid' },
+    'dashed': { value: 'dashed' },
+    'dotted': { value: 'dotted' },
+    'double': { value: 'double' },
+  }
+}
---
+++ web/themes/custom/cloudy/src/design-tokens/borderWidth.js
@@ -0,0 +1,9 @@
+module.exports = {
+  borderWidth: {
+    'default': { value: '1px' },
+    '0': { value: '0' },
+    '2': { value: '2' },
+    '4': { value: '4' },
+    '8': { value: '8' },
+  }
+}
