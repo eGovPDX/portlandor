27aceaa99a186e3d36c73306ace6b3818ea0ed73
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-04-28 15:15:27 -0700

    POWR-2001-1 another attempt at creating a working query

--- web/private/scripts/db_cleanup.php
+++ web/private/scripts/db_cleanup.php
@@ -3,82 +3,82 @@
 // destroy the canonical version of the data.
 if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
 
-	$webform_query = "truncate table webform_submission; truncate table webform_submission_data; truncate table webform_submission_log;";
-	$watchdog_query = "truncate table watchdog;";
 
 	// Delete webform submission data
+	$webform_query = "truncate table webform_submission; truncate table webform_submission_data; truncate table webform_submission_log;";
 	echo "Deleting webform submissions...\n";
 	passthru('drush sql-query "' . $webform_query . '"');
 	echo "Webform submission deletion complete.\n";
 
-	// Delete log data
-	echo "Deleting log entries...\n";
-	passthru('drush sql-query "' . $watchdog_query . '"');
-	echo "Log entry deletion complete.\n";
-
 	// Delete node revisions older than 30 days
-	// node revision delete
-	$node_revision_query = "		drop temporary table if exists tmp_old_node_revs;
-		create temporary table tmp_old_node_revs
-		select vid, date_format(from_unixtime(revision_timestamp), '%e %b %Y') AS 'date', revision_timestamp
-		from node_revision
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day))
-		order by revision_timestamp;
+	$revisions_query = "DROP TEMPORARY TABLE IF EXISTS candidate_nodes;
+CREATE TEMPORARY TABLE candidate_nodes
+SELECT n.nid, n.changed, r.vid, COUNT(*) AS total
+FROM
+node_field_data n
+INNER JOIN node_revision r ON r.nid = n.nid
+GROUP BY n.nid
+HAVING (COUNT(*) > 1);
 
-		delete C
-		from node_revision__field_body_content as C
-		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
+DROP TEMPORARY TABLE IF EXISTS expired_revisions;
+CREATE TEMPORARY TABLE expired_revisions
+SELECT t.vid AS vid
+FROM
+(SELECT r.vid AS vid, r.revision_timestamp AS revision_timestamp
+FROM candidate_nodes n
+INNER JOIN node_revision r ON r.nid = n.nid
+WHERE (changed < (CURRENT_DATE - INTERVAL 30 DAY)) AND ((n.vid <> r.vid))
+GROUP BY n.nid, r.vid, r.revision_timestamp
+ORDER BY revision_timestamp DESC
+LIMIT 9223372036854775807 OFFSET 0) t
+WHERE revision_timestamp < (CURRENT_DATE - INTERVAL 30 DAY);
 
-		delete C
-		from node_revision__field_summary as C
-		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
+DELETE C
+FROM node_revision__field_body_content C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		delete C
-		from node_revision__field_topics as C
-		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
+DELETE C
+FROM node_revision__field_summary C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		delete C
-		from node_revision__field_display_groups as C
-		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
+DELETE C
+FROM node_revision__field_topics C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		delete C
-		from node_revision__field_service_mode as C
-		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
+DELETE C
+FROM node_revision__field_display_groups C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		delete C
-		from node_revision__field_audience as C
-		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
+DELETE C
+FROM node_revision__field_service_mode C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		delete C
-		from node_revision__field_related_content as C
-		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
+DELETE C
+FROM node_revision__field_audience C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		delete C
-		from node_revision__field_popular as C
-		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
+DELETE C
+FROM node_revision__field_related_content C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		delete C
-		from node_revision__field_page_type as C
-		inner join tmp_old_node_revs REV on C.revision_id = REV.vid
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day)) and delta > 0;
+DELETE C
+FROM node_revision__field_popular C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		drop temporary table tmp_old_node_revs;
+DELETE C
+FROM node_revision__field_page_type C
+INNER JOIN expired_revisions REV ON C.revision_id = REV.vid;
 
-		-- now delete the revisions themselves
-		delete from node_revision
-		where revision_timestamp < unix_timestamp(date_sub(now(), interval 30 day));";
+DELETE R
+FROM node_revision R
+INNER JOIN expired_revisions REV ON R.vid = REV.vid;
 
-	// Delete log data
-	echo "Deleting node revisions older than 30 days...\n";
-	passthru('drush sql-query "' . $node_revision_query . '"');
-	echo "Node revision deletion complete.\n";
+DROP TEMPORARY TABLE IF EXISTS candidate_nodes;
+DROP TEMPORARY TABLE IF EXISTS expired_revisions;";
+
+	echo "Deleting revision data older than 30 days...\n";
+	passthru('drush sql-query "' . $revisions_query . '"');
+	echo "Revision deletion complete.\n";
 
 }
+
