90b5bc7fe0cc21e6b9349ac52876c450432369dd
Brittany Smart <brittanynsmart@gmail.com>
2020-09-17 10:21:21 -0700

    Config percy with circleci

--- web/themes/custom/cloudy/snapshots.js
+++ web/themes/custom/cloudy/snapshots.js
@@ -3,6 +3,6 @@ const PercyScript = require("@percy/script");
 PercyScript.run(async (page, percySnapshot) => {
   await page.goto("http://portlandor.lndo.site/");
   // ensure the page has loaded before capturing a snapshot
-  await page.waitFor(".menu");
+  await page.waitFor("header");
   await percySnapshot("homepage");
 });
