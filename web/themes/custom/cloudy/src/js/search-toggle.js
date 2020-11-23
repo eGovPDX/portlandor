dc87435361207c9743b0b3fade65c6f881cb2263
Brittany Smart <brittanynsmart@gmail.com>
2020-11-23 13:36:15 -0800

    powr-2718 WIP: search api block refactor

--- web/themes/custom/cloudy/src/js/search-toggle.js
+++ web/themes/custom/cloudy/src/js/search-toggle.js
@@ -3,44 +3,44 @@ import Drupal from 'Drupal';
 
 function closeSearchResults() {
   // Hide the search drawer when mobile menu toggle is clicked
-  const $searchToggle = $('.navbar-toggle-group .search-toggle');
+  const $searchToggle = $('#searchToggle');
   if ($searchToggle.attr('aria-expanded') === 'true') {
     $searchToggle.attr('aria-expanded', 'false');
-    $('.navbar-mobile-search-drawer').removeClass('show');
-    $(this).parent().parent('.navbar').removeClass('search-is-active');
+    // $('.navbar-mobile-search-drawer').toggleClass('show');
+    // $(this).parent().parent('.navbar').removeClass('search-is-active');
   }
 }
 
 Drupal.behaviors.search_toggle = {
   attach: function(context, settings) {
-    $(document, context).once('drawerOpenHandler').on('click', '.navbar-toggle-group .search-toggle', function(event) {
+    $(document, context).once('drawerOpenHandler').on('click', '#searchToggle', function(event) {
       event.preventDefault();
       event.stopPropagation();
 
       // Hide the mobile drawer when search toggle is clicked
-      const $mobileMenuToggle = $('.navbar-toggle-group .navbar-toggler');
+      const $mobileMenuToggle = $('#navToggle');
       if ($mobileMenuToggle.attr('aria-expanded') === 'true') {
         $mobileMenuToggle.addClass('collapsed').attr('aria-expanded', 'false');
-        $('.navbar-mobile-drawer').removeClass('show');
-      }
-
-      $(this).parent('.navbar-toggle-group').siblings('.navbar-mobile-search-drawer').toggleClass('show');
-      $(this).attr('aria-expanded', $(this).attr('aria-expanded') === 'false' ? 'true' : 'false');
-      $(this).parent().parent('.navbar').toggleClass('search-is-active');
+        $('#navbarNav').removeClass('show');
+			}
     });
   }
 };
 
 Drupal.behaviors.mobile_menu_toggle_search_close = {
   attach: function(context, settings) {
-    $(document, context).once('drawerCloseHandler').on('click', '.navbar-toggle-group .navbar-toggler', function(event) {
+    $(document, context).once('drawerCloseHandler').on('click', '#navToggle', function(event) {
       event.preventDefault();
       event.stopPropagation();
-      closeSearchResults();
+			closeSearchResults();
+
+			$(this).find('#navbar-toggle-item--open').toggleClass('d-none');
+			$(this).find('#navbar-toggle-item--close').toggleClass('d-block');
     });
   }
 };
 
+// TODO: search form close button
 Drupal.behaviors.close_search_results = {
   attach: function(context, settings) {
     $(document, context).once('drawerCloseResultsHandler').on('click', '.navbar-mobile-search-drawer .icon', function(event) {
