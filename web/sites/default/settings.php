4aa549a99d46238d7311fa7c003222639902d623
Kevin Wang <xwang@Kevins-MBP.hsd1.or.comcast.net>
2020-12-04 11:13:08 -0800

    POWR-3007 Enable Redis and add caching for Search API views

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -145,3 +145,19 @@ if (function_exists('newrelic_ignore_transaction') && php_sapi_name() === 'cli')
 }
 
 $config['file.settings']['make_unused_managed_files_temporary'] = TRUE;
+
+// Configure Redis
+if (defined('PANTHEON_ENVIRONMENT')) {
+  // Include the Redis services.yml file. Adjust the path if you installed to a contrib or other subdirectory.
+  $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+
+  //phpredis is built into the Pantheon application container.
+  $settings['redis.connection']['interface'] = 'PhpRedis';
+  // These are dynamic variables handled by Pantheon.
+  $settings['redis.connection']['host']      = $_ENV['CACHE_HOST'];
+  $settings['redis.connection']['port']      = $_ENV['CACHE_PORT'];
+  $settings['redis.connection']['password']  = $_ENV['CACHE_PASSWORD'];
+
+  $settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
+  $settings['cache_prefix']['default'] = 'pantheon-redis';
+}
