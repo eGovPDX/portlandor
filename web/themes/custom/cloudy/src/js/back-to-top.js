d4d3e80a0c922659286047fc8b4f00c9828dd7c1
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-12-08 15:42:17 -0800

    POWR-2985 show button based on scroll position

--- web/themes/custom/cloudy/src/js/back-to-top.js
+++ web/themes/custom/cloudy/src/js/back-to-top.js
@@ -1,4 +1,23 @@
 import $ from 'jquery';
 //import Drupal from 'Drupal';
 
-$('#block-cloudy-content').append('<div id="back-to-top"><a href="#main-content">Back to top</a></div>');
+
+var viewHeight = $(window).height();
+var showHeight = viewHeight * 2;
+var isAttached = false;
+
+$(window).on('scroll', function() {
+  var scrollPos = $(document).scrollTop();
+  if (scrollPos > showHeight && !isAttached) {
+    $('#block-cloudy-content').append('<div id="back-to-top"><a href="#main-content">Back to top</a></div>');
+    isAttached = true;
+  } else if (scrollPos <= showHeight && isAttached) {
+    $('#back-to-top').remove();
+    isAttached = false;
+  }
+});
+
+$('#back-to-top').on('click', function() {
+  $(this).remove();
+  isAttached = false;
+});
