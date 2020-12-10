dd7d750304f078517e1f8fea544e09ecfb823841
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-12-10 12:10:19 -0800

    POWR-2985 attach back-to-top library globally in the info file, fix style that changed after merge from demo branch, changed delay to 1.5 viewport heights

--- web/themes/custom/cloudy/src/js/back-to-top.js
+++ web/themes/custom/cloudy/src/js/back-to-top.js
@@ -3,13 +3,13 @@ import $ from 'jquery';
 
 
 var viewHeight = $(window).height();
-var showHeight = viewHeight * 2;
+var showHeight = viewHeight * 1.5;
 var isAttached = false;
 
 $(window).on('scroll', function() {
   var scrollPos = $(document).scrollTop();
   if (scrollPos > showHeight && !isAttached) {
-    $('#block-cloudy-content').append('<div id="back-to-top"><a href="#main-content">Back to top</a></div>');
+    $('#block-cloudy-content').append('<div id="back-to-top" class="btn btn-dark"><a href="#main-content">Back to top</a></div>');
     isAttached = true;
   } else if (scrollPos <= showHeight && isAttached) {
     $('#back-to-top').remove();
