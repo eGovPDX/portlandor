b7258def1e7a46911ed9f408c35299248cf1d965
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-12-11 12:51:03 -0800

    POWR-2985 code review updates after feedback from Brit, added context to jquery selectors, added Drupal.t to button text, changed behavior id to camel case

--- web/themes/custom/cloudy/src/js/back-to-top.js
+++ web/themes/custom/cloudy/src/js/back-to-top.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import Drupal from 'Drupal';
 
-Drupal.behaviors.cloudy_back_to_top = {
+Drupal.behaviors.cloudyBackToTop = {
   attach: function(context, settings) {
     var viewHeight = $(window).height();
     var showHeight = viewHeight * 1.5;
@@ -10,7 +10,8 @@ Drupal.behaviors.cloudy_back_to_top = {
     $(window).once('backToTopShowButtonHandler').on('scroll', function() {
       var scrollPos = $(document).scrollTop();
       if (scrollPos > showHeight && !isAttached) {
-        $('#block-cloudy-content').append('<div id="back-to-top" class="btn btn-dark"><a href="#main-content">Back to top</a></div>');
+        var buttonText = Drupal.t('Back to top');
+        $('#block-cloudy-content', context).append('<div id="back-to-top" class="btn btn-dark"><a href="#header">' + buttonText + '</a></div>');
         isAttached = true;
       } else if (scrollPos <= showHeight && isAttached) {
         $('#back-to-top').remove();
@@ -18,7 +19,7 @@ Drupal.behaviors.cloudy_back_to_top = {
       }
     });
 
-    $('#back-to-top').once('backToTopClickHandler').on('click', function() {
+    $('#back-to-top', context).once('backToTopClickHandler').on('click', function() {
       $(this).remove();
       isAttached = false;
     });
