ec921e9c9d6f3d5e12684dc060432e07e23616b3
Greg Clapp <gregory.clapp@portlandoregon.gov>
2020-12-11 09:55:55 -0800

    POWR-2985 best practices: added drupal behavior and once function

--- web/themes/custom/cloudy/src/js/back-to-top.js
+++ web/themes/custom/cloudy/src/js/back-to-top.js
@@ -1,23 +1,27 @@
 import $ from 'jquery';
-//import Drupal from 'Drupal';
+import Drupal from 'Drupal';
 
+Drupal.behaviors.cloudy_back_to_top = {
+  attach: function(context, settings) {
+    var viewHeight = $(window).height();
+    var showHeight = viewHeight * 1.5;
+    var isAttached = false;
 
-var viewHeight = $(window).height();
-var showHeight = viewHeight * 1.5;
-var isAttached = false;
+    $(window).on('scroll', function() {
+      var scrollPos = $(document).scrollTop();
+      if (scrollPos > showHeight && !isAttached) {
+        $('#block-cloudy-content').append('<div id="back-to-top" class="btn btn-dark"><a href="#main-content">Back to top</a></div>');
+        isAttached = true;
+      } else if (scrollPos <= showHeight && isAttached) {
+        $('#back-to-top').remove();
+        isAttached = false;
+      }
+    });
 
-$(window).on('scroll', function() {
-  var scrollPos = $(document).scrollTop();
-  if (scrollPos > showHeight && !isAttached) {
-    $('#block-cloudy-content').append('<div id="back-to-top" class="btn btn-dark"><a href="#main-content">Back to top</a></div>');
-    isAttached = true;
-  } else if (scrollPos <= showHeight && isAttached) {
-    $('#back-to-top').remove();
-    isAttached = false;
+    $('#back-to-top').on('click', function() {
+      $(this).remove();
+      isAttached = false;
+    });
   }
-});
+};
 
-$('#back-to-top').on('click', function() {
-  $(this).remove();
-  isAttached = false;
-});
