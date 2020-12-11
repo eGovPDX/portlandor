17fda5bfc308a92886daa647414a03b9a88c70be
Kevin Wang <xwang@Kevins-MBP.hsd1.or.comcast.net>
2020-12-11 14:13:54 -0800

    POWR-3007 Clean up after review

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -146,18 +146,24 @@ if (function_exists('newrelic_ignore_transaction') && php_sapi_name() === 'cli')
 
 $config['file.settings']['make_unused_managed_files_temporary'] = TRUE;
 
+
+////////////////////////////////////////////////////////////
+// Only uncomment the following lines in the next deployment after the Redis module is enabled on Pantheon.
+// Otherwise will get WSOD.
+////////////////////////////////////////////////////////////
+
 // Configure Redis
-if (defined('PANTHEON_ENVIRONMENT')) {
-  // Include the Redis services.yml file. Adjust the path if you installed to a contrib or other subdirectory.
-  $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+// if (defined('PANTHEON_ENVIRONMENT')) {
+//   // Include the Redis services.yml file. Adjust the path if you installed to a contrib or other subdirectory.
+//   $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
 
-  //phpredis is built into the Pantheon application container.
-  $settings['redis.connection']['interface'] = 'PhpRedis';
-  // These are dynamic variables handled by Pantheon.
-  $settings['redis.connection']['host']      = $_ENV['CACHE_HOST'];
-  $settings['redis.connection']['port']      = $_ENV['CACHE_PORT'];
-  $settings['redis.connection']['password']  = $_ENV['CACHE_PASSWORD'];
+//   //phpredis is built into the Pantheon application container.
+//   $settings['redis.connection']['interface'] = 'PhpRedis';
+//   // These are dynamic variables handled by Pantheon.
+//   $settings['redis.connection']['host']      = $_ENV['CACHE_HOST'];
+//   $settings['redis.connection']['port']      = $_ENV['CACHE_PORT'];
+//   $settings['redis.connection']['password']  = $_ENV['CACHE_PASSWORD'];
 
-  $settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
-  $settings['cache_prefix']['default'] = 'pantheon-redis';
-}
+//   $settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
+//   $settings['cache_prefix']['default'] = 'pantheon-redis';
+// }
