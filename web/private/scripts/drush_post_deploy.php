baaa68f0b9d98a668b3090400e986472591353a5
Kevin Wang <xinju.wang@portlandoregon.gov>
2021-02-16 15:38:48 -0800

    POWR-3208 Add -y to drush deploy

--- web/private/scripts/drush_post_deploy.php
+++ web/private/scripts/drush_post_deploy.php
@@ -7,5 +7,5 @@ if (preg_match('/^bot-\d+/', $_ENV['PANTHEON_ENVIRONMENT'])) {
 
 //Clear the cache to make sure database doesn't conflict
 echo "Start running drush deploy...\n";
-passthru('drush deploy -v 2>&1');
+passthru('drush deploy -v -y 2>&1');
 echo "Done running drush deploy.\n";
