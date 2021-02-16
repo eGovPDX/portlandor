e8e3ddf654830b6aad2c60af791ff26fb81c23da
Kevin Wang <xinju.wang@portlandoregon.gov>
2021-02-16 15:13:00 -0800

    POWR-3208 Use new drush commnd "deploy"

--- web/private/scripts/drush_post_deploy.php
+++ web/private/scripts/drush_post_deploy.php
@@ -5,8 +5,7 @@ if (preg_match('/^bot-\d+/', $_ENV['PANTHEON_ENVIRONMENT'])) {
     die('Dependabot branch detected. Aborting!');
 }
 
-
 //Clear the cache to make sure database doesn't conflict
-echo "Start rebuilding cache...\n";
-passthru('drush cr 2>&1');
-echo "Done rebuilding cache.\n";
+echo "Start running drush deploy...\n";
+passthru('drush deploy -v 2>&1');
+echo "Done running drush deploy.\n";
