0924d9abdce8a1d0b4c604e627e5d982a8480a9a
Kevin Wang <xinju.wang@portlandoregon.gov>
2021-04-08 10:50:18 -0700

    POWR-3363 Capture screenshot when test failed

--- tests/percy/test/snapshots-anonymous.js
+++ tests/percy/test/snapshots-anonymous.js
@@ -9,7 +9,7 @@ var browser, page;
 before(async () => {
   browser = await puppeteer.launch({
     ignoreHTTPSErrors: true,
-    args: [ '--no-sandbox'],
+    args: ['--no-sandbox'],
   })
   page = await browser.newPage()
   await page.setDefaultTimeout(30000);
@@ -17,50 +17,62 @@ before(async () => {
 
 describe('Anonymous user test', () => {
   it('has correct text on home page', async function () {
-    await page.goto(HOME_PAGE);
-    await page.waitForSelector('nav');
+    try {
+      await page.goto(HOME_PAGE);
+      await page.waitForSelector('nav');
 
-    // Verify the home page has text "Services"
-    let text_content = await page.evaluate(() => document.querySelector('nav').textContent);
-    assert(text_content.includes('Services'), 'Cannot find "Services" on Home page')
+      // Verify the home page has text "Services"
+      let text_content = await page.evaluate(() => document.querySelector('nav').textContent);
+      assert(text_content.includes('asdfServices'), 'Cannot find "Services" on Home page')
 
-    // Verify the home page has text "Services"
-    text_content = await page.evaluate(() => document.querySelector('div.content h2.h6').textContent);
-    assert(text_content === 'City of Portland, Oregon', 'Cannot find "City of Portland, Oregon" on Home page')
+      // Verify the home page has text "Services"
+      text_content = await page.evaluate(() => document.querySelector('div.content h2.h6').textContent);
+      assert(text_content === 'City of Portland, Oregon', 'Cannot find "City of Portland, Oregon" on Home page')
 
-    await percySnapshot(page, 'Anonymous - Home page');
+      await percySnapshot(page, 'Anonymous - Home page');
 
-    // Search page
-    await page.goto(`${HOME_PAGE}/search?keys=tax`);
-    await percySnapshot(page, 'Anonymous - Search "tax"');
+      // Search page
+      await page.goto(`${HOME_PAGE}/search?keys=tax`);
+      await percySnapshot(page, 'Anonymous - Search "tax"');
 
-    // 404
-    await page.goto(`${HOME_PAGE}/search?keys=powr-test`);
-    await percySnapshot(page, 'Anonymous - 404 "powr-test"');
+      // 404
+      await page.goto(`${HOME_PAGE}/search?keys=powr-test`);
+      await percySnapshot(page, 'Anonymous - 404 "powr-test"');
 
-    // Elected Officials
-    await page.goto(`${HOME_PAGE}/wheeler`);
-    await percySnapshot(page, 'Anonymous - Elected "Mayor"');
+      // Elected Officials
+      await page.goto(`${HOME_PAGE}/wheeler`);
+      await percySnapshot(page, 'Anonymous - Elected "Mayor"');
 
-    // Advisory Group
-    await page.goto(`${HOME_PAGE}/omf/toc`);
-    await percySnapshot(page, 'Anonymous - Advisory "Technology Oversight"');
+      // Advisory Group
+      await page.goto(`${HOME_PAGE}/omf/toc`);
+      await percySnapshot(page, 'Anonymous - Advisory "Technology Oversight"');
 
-    // Program
-    await page.goto(`${HOME_PAGE}/help`);
-    await percySnapshot(page, 'Anonymous - Program "POWR Help"');
+      // Program
+      await page.goto(`${HOME_PAGE}/help`);
+      await percySnapshot(page, 'Anonymous - Program "POWR Help"');
 
-    // Bureau
-    await page.goto(`${HOME_PAGE}/omf`);
-    await percySnapshot(page, 'Anonymous - Bureau "Mangagement and Finance"');
+      // Bureau
+      await page.goto(`${HOME_PAGE}/omf`);
+      await percySnapshot(page, 'Anonymous - Bureau "Mangagement and Finance"');
 
-    // Project
-    await page.goto(`${HOME_PAGE}/powr`);
-    await percySnapshot(page, 'Anonymous - Project "POWR"');
+      // Project
+      await page.goto(`${HOME_PAGE}/powr`);
+      await percySnapshot(page, 'Anonymous - Project "POWR"');
 
-    // Service
-    await page.goto(`${HOME_PAGE}/police/services/police-report-or-record-online-request`);
-    await percySnapshot(page, 'Anonymous - Service "Police Report or Record"');
+      // Service
+      await page.goto(`${HOME_PAGE}/police/services/police-report-or-record-online-request`);
+      await percySnapshot(page, 'Anonymous - Service "Police Report or Record"');
+    } catch (e) {
+      if(process.env.CIRCLECI) {
+        screenshot_path = "/var/www/html/artifacts/failedtests";
+        await page.screenshot({
+          path: screenshot_path + "/screenshot_anonymous_test_failure.jpg",
+          type: "jpeg",
+          fullPage: true
+        });
+      }
+      throw e; // re-throw the exception
+    }
   })
 });
 
