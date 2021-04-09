e1a73b184e2ef4f3b44636fbe93a37e6d5c9ccae
Brittany Smart <brittanynsmart@gmail.com>
2021-04-09 11:34:24 -0700

    POWR-3363 Add anonymous demo test

--- tests/percy/test/snapshots-anonymous.js
+++ tests/percy/test/snapshots-anonymous.js
@@ -1,81 +1,38 @@
-const percySnapshot = require('@percy/puppeteer')
-const puppeteer = require('puppeteer')
-const assert = require('assert');
+const percySnapshot = require("@percy/puppeteer");
+const puppeteer = require("puppeteer");
 
 const SITE_NAME = process.env.SITE_NAME;
-const HOME_PAGE = (SITE_NAME) ? `https://${SITE_NAME}-portlandor.pantheonsite.io` : 'https://portlandor.lndo.site';
+const HOME_PAGE = SITE_NAME
+  ? `https://${SITE_NAME}-portlandor.pantheonsite.io`
+  : "https://portlandor.lndo.site";
+const timeout = process.env.SLOWMO ? 30000 : 10000;
 
-var browser, page;
-before(async () => {
-  browser = await puppeteer.launch({
-    ignoreHTTPSErrors: true,
-    args: ['--no-sandbox'],
-  })
-  page = await browser.newPage()
-  await page.setDefaultTimeout(30000);
-})
+describe("Google", () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      // headless: false,
+      ignoreHTTPSErrors: true,
+      args: ["--no-sandbox"],
+    });
+    page = await browser.newPage();
+  });
 
-describe('Anonymous user test', () => {
-  it('has correct text on home page', async function () {
-    try {
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  it(
+    'title should display "City of Portland, Oregon | Portland.gov" text on homepage',
+    async () => {
       await page.goto(HOME_PAGE);
-      await page.waitForSelector('nav');
-
-      // Verify the home page has text "Services"
-      let text_content = await page.evaluate(() => document.querySelector('nav').textContent);
-      assert(text_content.includes('asdfServices'), 'Cannot find "Services" on Home page')
-
-      // Verify the home page has text "Services"
-      text_content = await page.evaluate(() => document.querySelector('div.content h2.h6').textContent);
-      assert(text_content === 'City of Portland, Oregon', 'Cannot find "City of Portland, Oregon" on Home page')
-
-      await percySnapshot(page, 'Anonymous - Home page');
-
-      // Search page
-      await page.goto(`${HOME_PAGE}/search?keys=tax`);
-      await percySnapshot(page, 'Anonymous - Search "tax"');
-
-      // 404
-      await page.goto(`${HOME_PAGE}/search?keys=powr-test`);
-      await percySnapshot(page, 'Anonymous - 404 "powr-test"');
-
-      // Elected Officials
-      await page.goto(`${HOME_PAGE}/wheeler`);
-      await percySnapshot(page, 'Anonymous - Elected "Mayor"');
-
-      // Advisory Group
-      await page.goto(`${HOME_PAGE}/omf/toc`);
-      await percySnapshot(page, 'Anonymous - Advisory "Technology Oversight"');
-
-      // Program
-      await page.goto(`${HOME_PAGE}/help`);
-      await percySnapshot(page, 'Anonymous - Program "POWR Help"');
-
-      // Bureau
-      await page.goto(`${HOME_PAGE}/omf`);
-      await percySnapshot(page, 'Anonymous - Bureau "Mangagement and Finance"');
-
-      // Project
-      await page.goto(`${HOME_PAGE}/powr`);
-      await percySnapshot(page, 'Anonymous - Project "POWR"');
-
-      // Service
-      await page.goto(`${HOME_PAGE}/police/services/police-report-or-record-online-request`);
-      await percySnapshot(page, 'Anonymous - Service "Police Report or Record"');
-    } catch (e) {
-      if(process.env.CIRCLECI) {
-        screenshot_path = "/home/circleci/artifacts";
-        await page.screenshot({
-          path: screenshot_path + "/screenshot_anonymous_test_failure.jpg",
-          type: "jpeg",
-          fullPage: true
-        });
-      }
-      throw e; // re-throw the exception
-    }
-  })
+      let text_content = await page.evaluate(
+        () => document.querySelector("h1").textContent
+      );
+      expect(text_content).toEqual("Welcome to Portland, Oregon");
+      await percySnapshot(page, "Anonymous - Home page");
+    },
+    timeout
+  );
 });
-
-after(async () => {
-  await browser.close()
-})
