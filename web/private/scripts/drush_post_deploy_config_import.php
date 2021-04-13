3d865140e2a1f56b4622241e78c459ed795ba992
Joshua Mitchell <josh@joshuami.com>
2021-04-13 12:10:26 -0700

    POWR-3414 add a timestamp to the post_deploy_config_import

--- web/private/scripts/drush_post_deploy_config_import.php
+++ web/private/scripts/drush_post_deploy_config_import.php
@@ -8,5 +8,7 @@ if (preg_match('/^bot-\d+/', $_ENV['PANTHEON_ENVIRONMENT'])) {
 
 // Import all config changes.
 echo "Start importing configuration from yml files...\n";
+system('date +"%T"');
 system('drush config-import -y -vvv 2>&1');
+system('date +"%T"');
 echo "Done importing of configuration.\n";
