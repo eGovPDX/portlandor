1ae1d42223f2cb4bfd64052ec68c3b549d6235dd
Joshua Mitchell <josh@joshuami.com>
2021-04-13 10:41:35 -0700

    POWR-3414 add maintenance mode on and off

---
+++ web/private/scripts/drush_post_deploy_maintenance_on.php
@@ -0,0 +1,12 @@
+<?php
+
+// Do NOT run for Dependabot branches
+if (preg_match('/^bot-\d+/', $_ENV['PANTHEON_ENVIRONMENT'])) {
+    die('Dependabot branch detected. Aborting!');
+}
+
+
+// Turn on maintenance mode.
+echo "Turn on maintenance mode...\n";
+system('drush drush sset system.maintenance_mode TRUE 2>&1');
+echo "Maintenance mode is on.\n";
