cf9ea7e7d8b7629f28694a6b10c5c63d7ae73a70
Joshua Mitchell <josh@joshuami.com>
2021-04-23 14:29:53 -0700

    POWR-3439 remove unused bot-check, clean up pantheon.yml and unused scripts

--- web/private/scripts/drush_post_deploy_maintenance_off.php
+++ web/private/scripts/drush_post_deploy_maintenance_off.php
@@ -1,11 +1,5 @@
 <?php
 
-// Do NOT run for Dependabot branches
-if (preg_match('/^bot-\d+/', $_ENV['PANTHEON_ENVIRONMENT'])) {
-    die('Dependabot branch detected. Aborting!');
-}
-
-
 // Turn off maintenance mode.
 echo "Turn off maintenance mode...\n";
 system('drush sset system.maintenance_mode FALSE 2>&1');
