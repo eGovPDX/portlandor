d3bb10cc8fc405f5cd8eded8bac061d365794556
Kevin Wang <xinju.wang@portlandoregon.gov>
2021-05-06 14:02:34 -0700

    POWR-3411 Add Percy snapshots

--- tests/percy/__tests__/anonymous.js
+++ tests/percy/__tests__/anonymous.js
@@ -5,9 +5,9 @@ const SITE_NAME = process.env.SITE_NAME;
 const HOME_PAGE = SITE_NAME
   ? `https://${SITE_NAME}-portlandor.pantheonsite.io`
   : "https://portlandor.lndo.site";
-const timeout = 30000;
+const timeout = 60000*2;
 
-var BROWSER_OPTION = { 
+var BROWSER_OPTION = {
   ignoreHTTPSErrors: true,
   args: ["--no-sandbox"],
 };
@@ -24,9 +24,9 @@ beforeAll(async () => {
   page = await browser.newPage();
 
   // Print browser version
-  // await page.browser().version().then(function(version) {
+  // await page.browser().version().then(function (version) {
   //   console.log(version);
-  //   });
+  // });
   // console.log(browser.process().spawnfile);
 }, timeout)
 
@@ -68,6 +68,36 @@ describe("Homepage", () => {
       await page.type("#edit-keys", "powr");
       await page.waitForSelector("#ui-id-1", { visible: true });
       await percySnapshot(page, "Anonymous - Home Autocomplete");
+    },
+    timeout
+  );
+
+  it(
+    "Take snapshots of Group home pages",
+    async () => {
+      await page.goto(`${HOME_PAGE}/omf`, { waitUntil: "load" });
+      await percySnapshot(page, "Anonymous - Bureau \"Mangagement and Finance\"");
+      await page.goto(`${HOME_PAGE}/powr`, { waitUntil: "load" });
+      await percySnapshot(page, "Anonymous - Project \"POWR\"");
+      await page.goto(`${HOME_PAGE}/help`, { waitUntil: "load" });
+      await percySnapshot(page, "Anonymous - Program \"POWR help\"");
+      await page.goto(`${HOME_PAGE}/omf/toc`, { waitUntil: "load" });
+      await percySnapshot(page, "Anonymous - Advisory \"Technology Oversight\"");
+      await page.goto(`${HOME_PAGE}/mayor`, { waitUntil: "load" });
+      await percySnapshot(page, "Anonymous - Elected \"Mayor\"");
+    },
+    timeout
+  );
+
+  it(
+    "Take snapshots of search results and content pages",
+    async () => {
+      await page.goto(`${HOME_PAGE}/search?keys=tax&op=`, { waitUntil: "load" });
+      await percySnapshot(page, "Anonymous - Search \"tax\"");
+      await page.goto(`${HOME_PAGE}/search?keys=powr-test&op=`, { waitUntil: "load" });
+      await percySnapshot(page, "Anonymous - Search \"powr-test\"");
+      await page.goto(`${HOME_PAGE}/police/report-or-record-request`, { waitUntil: "load" });
+      await percySnapshot(page, "Anonymous - Service \"Police Report or Record\"");
     },
     timeout
   );
