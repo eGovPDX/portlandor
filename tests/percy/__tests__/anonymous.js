edb186a6e965e20d69ae336b83b04abb8c05dcdc
Kevin Wang <xinju.wang@portlandoregon.gov>
2021-05-10 13:33:53 -0700

    Restore test

--- tests/percy/__tests__/anonymous.js
+++ tests/percy/__tests__/anonymous.js
@@ -45,7 +45,7 @@ describe("Homepage", () => {
       );
       // Compares it with the intended behavior
       expect(title).toBe("Welcome to Portland, Oregon");
-//      await percySnapshot(page, "Anonymous - Home page");
+      await percySnapshot(page, "Anonymous - Home page");
     },
     timeout
   );
@@ -56,7 +56,7 @@ describe("Homepage", () => {
       await page.goto(HOME_PAGE, { waitUntil: "load" });
       await page.click("button.cloudy-header__toggle--menu");
       await page.waitForSelector(".cloudy-header__menu-wrapper.show");
-//      await percySnapshot(page, "Anonymous - Home page Menu");
+      await percySnapshot(page, "Anonymous - Home page Menu");
     },
     timeout
   );
@@ -67,7 +67,7 @@ describe("Homepage", () => {
       await page.goto(HOME_PAGE, { waitUntil: "load" });
       await page.type("#edit-keys", "powr");
       await page.waitForSelector("#ui-id-1", { visible: true });
-//      await percySnapshot(page, "Anonymous - Home Autocomplete");
+      await percySnapshot(page, "Anonymous - Home Autocomplete");
     },
     timeout
   );
@@ -76,15 +76,15 @@ describe("Homepage", () => {
     "Take snapshots of Group home pages",
     async () => {
       await page.goto(`${HOME_PAGE}/omf`, { waitUntil: "load" });
-//      await percySnapshot(page, "Anonymous - Bureau \"Mangagement and Finance\"");
+      await percySnapshot(page, "Anonymous - Bureau \"Mangagement and Finance\"");
       await page.goto(`${HOME_PAGE}/powr`, { waitUntil: "load" });
-//      await percySnapshot(page, "Anonymous - Project \"POWR\"");
+      await percySnapshot(page, "Anonymous - Project \"POWR\"");
       await page.goto(`${HOME_PAGE}/help`, { waitUntil: "load" });
-//      await percySnapshot(page, "Anonymous - Program \"POWR Help\"");
+      await percySnapshot(page, "Anonymous - Program \"POWR Help\"");
       await page.goto(`${HOME_PAGE}/omf/toc`, { waitUntil: "load" });
-//      await percySnapshot(page, "Anonymous - Advisory \"Technology Oversight\"");
+      await percySnapshot(page, "Anonymous - Advisory \"Technology Oversight\"");
       await page.goto(`${HOME_PAGE}/mayor`, { waitUntil: "load" });
-//      await percySnapshot(page, "Anonymous - Elected \"Mayor\"");
+      await percySnapshot(page, "Anonymous - Elected \"Mayor\"");
     },
     timeout
   );
@@ -93,11 +93,11 @@ describe("Homepage", () => {
     "Take snapshots of search results and content pages",
     async () => {
       await page.goto(`${HOME_PAGE}/search?keys=tax&op=`, { waitUntil: "load" });
-//      await percySnapshot(page, "Anonymous - Search \"tax\"");
+      await percySnapshot(page, "Anonymous - Search \"tax\"");
       await page.goto(`${HOME_PAGE}/search?keys=powr-test&op=`, { waitUntil: "load" });
-//      await percySnapshot(page, "Anonymous - 404 \"powr-test\"");
+      await percySnapshot(page, "Anonymous - 404 \"powr-test\"");
       await page.goto(`${HOME_PAGE}/police/report-or-record-request`, { waitUntil: "load" });
-//      await percySnapshot(page, "Anonymous - Service \"Police Report or Record\"");
+      await percySnapshot(page, "Anonymous - Service \"Police Report or Record\"");
     },
     timeout
   );
