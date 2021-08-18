2672371da9ca9169dd12e056099dcb07b5ddd267
Kevin Wang <xinju.wang@portlandoregon.gov>
2021-08-18 11:22:37 -0700

    Use search query on Graph API

--- web/modules/custom/portland/modules/portland_openid_connect/src/Plugin/Action/BlockUserByADStatus.php
+++ web/modules/custom/portland/modules/portland_openid_connect/src/Plugin/Action/BlockUserByADStatus.php
@@ -24,17 +24,25 @@ class BlockUserByADStatus extends ActionBase
   {
     if (empty($account)) return;
 
+    // Skip if the Drupal user is marked as "Is Distribution List"
+    $users = \Drupal::entityTypeManager()->getStorage('user')
+    ->loadByProperties(['mail' => $account->getEmail()]);
+    if( empty($users) ) return;
+    if( array_values($users)[0]->field_is_distribution_list->value ) return;
+
+    // Never block these users
     $skip_emails = [
       'BTS-eGov@portlandoregon.gov',
       'ally.admin@portlandoregon.gov',
       'marty.member@portlandoregon.gov',
-      'amy.archer-masters@portlandoregon.gov',
-      'amy.archer@portlandoregon.gov',
-      'Aaron.ScottMerrion@portlandoregon.gov',
-      'WBUDFTeam@portlandoregon.gov',
-      'council140@portlandoregon.gov'
+      'oliver.outsider@portlandoregon.gov',
+      // 'amy.archer-masters@portlandoregon.gov',  // User email address
+      // 'amy.archer@portlandoregon.gov',  // Principal user name
+      // 'WBUDFTeam@portlandoregon.gov',  // Outlook distribution list
+      // 'council140@portlandoregon.gov',  // Actual AD group
     ];
-    if( in_array(strtolower($account->getEmail()), array_map('strtolower', $skip_emails)) ) return;
+    if (in_array(strtolower($account->getEmail()), array_map('strtolower', $skip_emails))) return;
+
     $tokens = BlockUserByADStatus::GetAccessToken();
     if (empty($tokens) || empty($tokens['access_token'])) {
       \Drupal::logger('portland OpenID')->error("Cannot retrieve access token for Microsoft Graph. Make sure the client secret is correct.");
@@ -71,36 +79,44 @@ class BlockUserByADStatus extends ActionBase
       'headers' => [
         'Content-Type' => 'application/json',
         'Authorization' => 'Bearer ' . $access_token,
+        'ConsistencyLevel' => 'eventual', // required by Graph search API
       ],
     ];
 
     $found_user_in_AD = false;
     try {
-      $response = $client->get('https://graph.microsoft.com/v1.0/users/' . $email, $options);
+      // Document: https://docs.microsoft.com/en-us/graph/query-parameters?context=graph%2Fapi%2F1.0&view=graph-rest-1.0
+      // Example: https://graph.microsoft.com/v1.0/users?$count=true&$search="mail:xinju.wang@portlandoregon.gov"
+      $response = $client->get(
+        'https://graph.microsoft.com/v1.0/users?$count=true&$search="mail:' . $email . '"',
+        $options
+      );
 
       $response_data = json_decode((string) $response->getBody(), TRUE);
       // If we cannot find the user in AD by email, assume we need to block the user in Drupal
-      $found_user_in_AD = isset($response_data['mail']);
+      $found_user_in_AD = isset($response_data['@odata.count']) && ($response_data['@odata.count'] > 0);
     } catch (RequestException $e) {
-    }
-
-    if (!$found_user_in_AD) {
-      // Find the user with email
-      $users = \Drupal::entityTypeManager()->getStorage('user')
-        ->loadByProperties(['mail' => $email]);
-
-      if (count($users) != 0) {
-        $user = array_values($users)[0]; // Assume the lookup returns only one unique user.
-        $user->status = 0;
-        $user->save();
-        \Drupal::logger('portland OpenID')->notice('User blocked: ' . $user->mail->value);
-      }
-
       $variables = [
         '@message' => 'Could not retrieve user information',
         '@error_message' => $e->getMessage(),
       ];
       \Drupal::logger('portland OpenID')->error('@message. Details: @error_message', $variables);
+    }
+
+    if (!$found_user_in_AD) {
+      // Load the Drupal user with email
+      $users = \Drupal::entityTypeManager()->getStorage('user')
+        ->loadByProperties(['mail' => $email]);
+
+      if (count($users) != 0) {
+        $user = array_values($users)[0]; // Assume the lookup returns only one unique user.
+        if ($user->status->value) {
+          $user->status = 0;
+          $user->save();
+          \Drupal::logger('portland OpenID')->notice('User blocked: ' . $user->mail->value);
+        }
+      }
+
       return $this->t('User blocked');
     }
   }
