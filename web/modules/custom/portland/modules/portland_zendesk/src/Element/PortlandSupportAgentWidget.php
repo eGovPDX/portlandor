acc0de18639365cec3134912741c994f64d1ce33
Greg Clapp <gregory.clapp@portlandoregon.gov>
2022-09-19 15:25:58 -0700

    CS-285 set access permissions on composite element at the parent level

--- web/modules/custom/portland/modules/portland_zendesk/src/Element/PortlandSupportAgentWidget.php
+++ web/modules/custom/portland/modules/portland_zendesk/src/Element/PortlandSupportAgentWidget.php
@@ -31,6 +31,8 @@ class PortlandSupportAgentWidget extends WebformCompositeBase {
    * NOTE: custom elements must have a #title attribute. if a value is not set here, it must be set
    * in the field config. if not, an error is thrown when trying to add an email handler.
    * 
+   * When configuring this element, need to set the access to be Administrator and Support Agent roles only. 
+   * 
    * How to programmatically set field conditions: https://www.drupal.org/docs/drupal-apis/form-api/conditional-form-fields
    */
   public static function getCompositeElements(array $element) {
@@ -40,42 +42,31 @@ class PortlandSupportAgentWidget extends WebformCompositeBase {
     $currentUserName = $currentUser->getDisplayName();
 
     $elements = [];
+    $elements['#access'] = ['support_agent', 'administrator'];
     $elements['#title'] = ['Support Agent Widget'];
     $elements['support_agent_widget_title'] = [
       '#type' => 'markup',
       '#title' => t('Support Agent Widget'),
       '#title_display' => 'invisible',
       '#markup' => '<h2>Customer Service Use Only</h2>',
-      '#access_create_roles' => ['support_agent', 'administrator'],
-      '#access_update_roles' => ['support_agent', 'administrator'],
-      '#access_view_roles' => ['support_agent', 'administrator'],
     ];
     $elements['employee_email'] = [
       '#type' => 'textfield',
       '#title' => t('Employee Email'),
       '#id' => 'employee_email',
       '#value' => $currentUserName . ' <[' . $currentUserEmail . ']>',
-      '#access_create_roles' => ['support_agent', 'administrator'],
-      '#access_update_roles' => ['support_agent', 'administrator'],
-      '#access_view_roles' => ['support_agent', 'administrator'],
     ];
     $elements['zendesk_request_number'] = [
       '#type' => 'number',
       '#title' => t('Zendesk Request Number'),
       '#id' => 'zendesk_request_number',
       '#description' => 'If you are completing this webform on behalf of a community member, please enter the Zendesk request number of the request created to track the interaction. In addition to creating a new request for this report, the existing interaction request will be updated and linked.',
-      '#access_create_roles' => ['support_agent', 'administrator'],
-      '#access_update_roles' => ['support_agent', 'administrator'],
-      '#access_view_roles' => ['support_agent', 'administrator'],
     ];
     $elements['test_submission'] = [
       '#type' => 'checkbox',
       '#title' => t('Test Submission'),
       '#id' => 'test_submission',
       '#description' => 'For administrtor use only. Handlers can be configured to process form submissions differently based on whether this box is checked.',
-      '#access_create_roles' => ['administrator'],
-      '#access_update_roles' => ['administrator'],
-      '#access_view_roles' => ['administrator'],
     ];
 
     return $elements;
