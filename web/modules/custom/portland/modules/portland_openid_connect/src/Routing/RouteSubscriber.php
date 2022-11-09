c41d1dafb4a2098f2678867cb906dd707926c2ee
Kevin Wang <xinju.wang@portlandoregon.gov>
2022-11-09 09:11:27 -0800

    PGOV-744 Replace openid login form

--- web/modules/custom/portland/modules/portland_openid_connect/src/Routing/RouteSubscriber.php
+++ web/modules/custom/portland/modules/portland_openid_connect/src/Routing/RouteSubscriber.php
@@ -66,9 +66,9 @@ class RouteSubscriber extends RouteSubscriberBase {
         'pgov-744', 'demo', 'dev', 'test', 'live'])
     ) {
       // only log in with an OpenID provider
-      if ($route = $collection->get('user.login')) {
-        $route->setDefault('_form', 'Drupal\openid_connect\Form\LoginForm');
-      }
+      // if ($route = $collection->get('user.login')) {
+      //   $route->setDefault('_form', 'Drupal\openid_connect\Form\LoginForm');
+      // }
       // don't accept POSTs to a login route
       if ($route = $collection->get('user.login.http')) {
         $route->setRequirement('_access', 'FALSE');
