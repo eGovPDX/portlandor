91476bedac5aa34472e2c69f6a615055250c3858
Oden <hello@odensc.com>
2022-12-01 12:08:11 -0800

    Revert "PGOV-769 Enable SAML SSO (#2424)"
    This reverts commit a09e125cbcde5e93164202b9c3cab7d5d18886a3.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -94,9 +94,6 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
   }
 }
 
-// Override the SP Entity ID value for each environment
-$config['samlauth.authentication']['sp_entity_id'] = $primary_domain;
-
 // Enable/disable config_split configurations based on the current environment
 $config['config_split.config_split.config_multidev']['status'] = FALSE;
 $config['config_split.config_split.config_dev']['status'] = FALSE;
