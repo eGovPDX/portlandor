0e46226cb0c57ec4e6970310c07024290abb5f4f
Josh Mitchell <joshuami@users.noreply.github.com>
2022-12-21 10:27:54 -0800

    PGOV-792 disabled portland_file_replace and custom code causing removal of translated documents

--- web/modules/custom/portland/modules/portland_file_replace/src/Plugin/Field/FieldWidget/FileWidget.php
+++ web/modules/custom/portland/modules/portland_file_replace/src/Plugin/Field/FieldWidget/FileWidget.php
@@ -13,24 +13,24 @@ use Drupal\file\Plugin\Field\FieldWidget\FileWidget as CoreFileWidget;
  *   }
  * )
  */
-class FileWidget extends CoreFileWidget {
-  public static function process($element, FormStateInterface $form_state, $form) {
-    $processed_element = parent::process($element, $form_state, $form);
+// class FileWidget extends CoreFileWidget {
+//   public static function process($element, FormStateInterface $form_state, $form) {
+//     $processed_element = parent::process($element, $form_state, $form);
 
-    // If there is already a file, add "replace file" link
-    if( !empty($element['#default_value']['fids']) ) {
-      $fid = $element['#default_value']['fids'][0];
-      $file = \Drupal\file\Entity\File::load($fid);
-      $user = \Drupal::currentUser();
-      // Only show replace link on permanent files. file_replace cannot replace temporary files.
-      if( $file != null && $user != null && $file->isPermanent() && $user->hasPermission('replace files') ) {
-        $processed_element['replace_link'] = [
-          '#type' => 'markup',
-          '#markup' => '<a target= "_blank" href="/admin/content/files/replace/' . 
-          $fid . '">Replace file and keep the original name</a>',
-        ];
-      }
-    }
-    return $processed_element;
-  }
-}
+//     // If there is already a file, add "replace file" link
+//     if( !empty($element['#default_value']['fids']) ) {
+//       $fid = $element['#default_value']['fids'][0];
+//       $file = \Drupal\file\Entity\File::load($fid);
+//       $user = \Drupal::currentUser();
+//       // Only show replace link on permanent files. file_replace cannot replace temporary files.
+//       if( $file != null && $user != null && $file->isPermanent() && $user->hasPermission('replace files') ) {
+//         $processed_element['replace_link'] = [
+//           '#type' => 'markup',
+//           '#markup' => '<a target= "_blank" href="/admin/content/files/replace/' . 
+//           $fid . '">Replace file and keep the original name</a>',
+//         ];
+//       }
+//     }
+//     return $processed_element;
+//   }
+// }
