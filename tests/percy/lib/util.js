501e2a1f25fd7d874a638dcba13ee6aa6ccc939c
Oden <hello@odensc.com>
2023-02-02 12:39:07 -0800

    PGOV-794 Bootstrap 5 migration work

--- tests/percy/lib/util.js
+++ tests/percy/lib/util.js
@@ -4,7 +4,7 @@ const path = require ('path');
 exports.masqueradeAs = async function(email, page, HOME_PAGE) {
   var encodedEmail = encodeURI(email);
   // Find the user in people list
-  await page.goto(`${HOME_PAGE}/admin/people?user=${encodedEmail}&status=All&role=All&permission=All`);
+  await page.goto(`${HOME_PAGE}/admin/people/list?user=${encodedEmail}&status=All&role=All&permission=All`);
   var link = await page.evaluate(() => document.querySelector('li.masquerade a').getAttribute('href'));
   await page.goto(`${HOME_PAGE}${link}`);
 }
