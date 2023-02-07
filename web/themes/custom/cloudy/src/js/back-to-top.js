a0f5967ba5f948c7f26e77e40b3f612863f2e18b
Oden <hello@odensc.com>
2023-02-07 14:58:53 -0800

    PGOV-794 Cleanup back to top button classes

--- web/themes/custom/cloudy/src/js/back-to-top.js
+++ web/themes/custom/cloudy/src/js/back-to-top.js
@@ -10,19 +10,19 @@
       var viewHeight = $(window).height();
       var showHeight = viewHeight * 1.5;
       var isAttached = false;
-  
+
       $(window).once('backToTopShowButtonHandler').on('scroll', function() {
         var scrollPos = $(document).scrollTop();
         if (scrollPos > showHeight && !isAttached) {
           var buttonText = Drupal.t('Back to top');
-          $('#block-cloudy-content', context).append('<div id="back-to-top" class="btn cloudy-btn-purple"><a href="#header">' + buttonText + '</a></div>');
+          $('#block-cloudy-content', context).append('<a id="back-to-top" class="btn btn-dark" href="#header">' + buttonText + '</a>');
           isAttached = true;
         } else if (scrollPos <= showHeight && isAttached) {
           $('#back-to-top').remove();
           isAttached = false;
         }
       });
-  
+
       $('#back-to-top', context).once('backToTopClickHandler').on('click', function() {
         $(this).remove();
         isAttached = false;
