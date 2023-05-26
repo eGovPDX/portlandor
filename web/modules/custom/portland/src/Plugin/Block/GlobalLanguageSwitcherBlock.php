fe6a6550af8c3e0a021d31e0e97c7cd40f31bb9d
Oden <hello@odensc.com>
2023-05-26 14:59:46 -0700

    PGOV-902 Implement dropdown menu

--- web/modules/custom/portland/src/Plugin/Block/GlobalLanguageSwitcherBlock.php
+++ web/modules/custom/portland/src/Plugin/Block/GlobalLanguageSwitcherBlock.php
@@ -42,13 +42,17 @@ class GlobalLanguageSwitcherBlock extends BlockBase implements ContainerFactoryP
    */
   public function build() {
     $current_langcode = $this->languageManager->getCurrentLanguage(LanguageInterface::TYPE_CONTENT)->getId();
-    // get the English version of the current URL. we'll always want to translate *from* English for best accuracy.
-    $current_url = \Drupal::request()->getSchemeAndHttpHost() . \Drupal::request()->getRequestUri();
+    // get the current full URL and remove any langcode from the path.
+    // we always want to translate *from* English for the best accuracy.
+    $current_url = \Drupal::request()->getSchemeAndHttpHost() . preg_replace("/^\/" . $current_langcode . "(\/|$)/", "$1", \Drupal::request()->getRequestUri());
     $languages = array_map(fn ($lang) => [
       'id' => $lang->getId(),
       'label' => $lang->getName(),
       'url' => "https://translate.google.com/translate?sl=en&tl={$lang->getId()}&u={$current_url}",
     ], $this->languageManager->getNativeLanguages());
+
+    // Chuukese is not supported by Google Translate
+    unset($languages['chk']);
 
     return [
       '#theme' => 'portland_global_language_switcher_block',
--- web/themes/custom/cloudy/src/js/bootstrap.js
+++ web/themes/custom/cloudy/src/js/bootstrap.js
@@ -1,2 +1,3 @@
 import 'bootstrap/js/src/collapse';
+import 'bootstrap/js/src/dropdown';
 import 'bootstrap/js/src/tab';
