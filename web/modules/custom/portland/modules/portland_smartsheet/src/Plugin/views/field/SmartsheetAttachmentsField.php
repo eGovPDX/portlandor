7a3f749ddf5b05eefe739c09b2d0b0da28a3b7a5
Oden <hello@odensc.com>
2023-09-13 18:07:15 -0700

    PGOV-945 trim filenames on overall testimony view, fix line breaks on testimony item view

--- web/modules/custom/portland/modules/portland_smartsheet/src/Plugin/views/field/SmartsheetAttachmentsField.php
+++ web/modules/custom/portland/modules/portland_smartsheet/src/Plugin/views/field/SmartsheetAttachmentsField.php
@@ -3,6 +3,8 @@
 namespace Drupal\portland_smartsheet\Plugin\views\field;
 
 use Drupal\Core\Url;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Unicode;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
 use Drupal\portland_smartsheet\Controller\AttachmentDownloadController;
@@ -13,6 +15,31 @@ use Drupal\portland_smartsheet\Controller\AttachmentDownloadController;
  * @ViewsField("smartsheet_attachments")
  */
 class SmartsheetAttachmentsField extends FieldPluginBase {
+    /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+
+    $options['trim_filename_chars'] = ['default' => 0];
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+
+    $form['trim_filename_chars'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Trim filenames to a maximum amount of characters'),
+      '#description' => $this->t('If greater than 0, filenames will be trimmed to this amount of chars (including ellipsis), with an ellipsis appended.'),
+      '#default_value' => $this->options['trim_filename_chars'],
+      '#required' => false
+    ];
+  }
+
   /**
    * {@inheritdoc}
    */
@@ -34,13 +61,17 @@ class SmartsheetAttachmentsField extends FieldPluginBase {
     if (!isset($row->cells['_data']->attachments)) return '';
 
     $sheet_id = $this->query->options['sheet_id'];
+    $trim_filename_chars = $this->options['trim_filename_chars'];
     return [
       '#theme' => 'item_list',
       '#list_type' => 'ul',
       '#items' => array_map(
         fn($attachment) => [
           '#theme' => 'cloudy_file_link',
-          '#file_display_name' => $attachment->name,
+          '#file_display_name' =>
+            $trim_filename_chars > 0
+              ? Unicode::truncate($attachment->name, $trim_filename_chars, false, true)
+              : $attachment->name,
           '#mime_type' => $attachment->mimeType,
           '#document_size' => $attachment->sizeInKb * 1024,
           '#document_link' => Url::fromRoute(
