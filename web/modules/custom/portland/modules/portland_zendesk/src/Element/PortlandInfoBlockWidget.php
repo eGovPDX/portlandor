53954df08818a096c20a69a770a46130884a33d9
Greg Clapp <gregory.clapp@portlandoregon.gov>
2023-12-06 13:21:11 -0800

    SIT-796 incremental update of work in progress so I can switch branches

--- web/modules/custom/portland/modules/portland_zendesk/src/Element/PortlandInfoBlockWidget.php
+++ web/modules/custom/portland/modules/portland_zendesk/src/Element/PortlandInfoBlockWidget.php
@@ -2,12 +2,11 @@
 
 namespace Drupal\portland_zendesk\Element;
 
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\webform\Element\WebformCompositeBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a 'portland_info_block_widget' element.
- *
  *
  * @FormElement("portland_info_block_widget")
  */
@@ -15,85 +14,45 @@ class PortlandInfoBlockWidget extends WebformCompositeBase {
 
   /**
    * {@inheritdoc}
-   * NOTE: custom elements must have a #title attribute. if a value is not set here, it must be set
-   * in the field config. if not, an error is thrown when trying to add an email handler (why?).
-   * 
-   * When configuring this element, need to set the access to be Administrator and Support Agent roles only. 
-   * 
-   * How to programmatically set field conditions: https://www.drupal.org/docs/drupal-apis/form-api/conditional-form-fields
    */
-  public static function getCompositeElements(array $element) {
+  public function getInfo() {
+    return parent::getInfo() + [
+      '#process' => ['::process'],
+    ];
+  }
 
-    $element['#title'] = ['Info Block Widget'];
-    
-    $form['attributes']['markup_headline'] = [
+  /**
+   * {@inheritdoc}
+   */
+  public static function processElement(array &$element, FormStateInterface $form_state, array &$complete_form) {
+    parent::processElement($element, $form_state, $complete_form);
+    // Add any additional processing here.
+
+    // Add title textfield.
+    $element['title'] = [
       '#type' => 'textfield',
-      '#title' => t('Headline'),
-      // Add more settings as needed.
+      '#title' => t('Info Title'),
+      '#required' => TRUE,
     ];
 
-    $form['attributes']['markup_body'] = [
-      '#type' => 'textarea',
-      '#title' => t('Body'),
-      // Add more settings as needed.
-    ];
-
-    $form['attributes']['markup_style'] = [
-      '#type' => 'select',
-      '#title' => t('Style'),
-      // Add more settings as needed.
-    ];
-
-    $form['attributes']['markup_body'] = [
+    // Add textarea for rich text editing.
+    $element['body'] = [
       '#type' => 'text_format',
-      '#title' => t('Body'),
-      '#format' => 'simple_editor',
+      '#title' => t('Info Body'),
+      '#required' => TRUE,
+      '#format' => 'full_html', // Adjust the text format as needed.
     ];
 
-
-    // $element['info_block_widget_title'] = [
-    //   '#type' => 'markup',
-    //   '#title' => t('Support Agent Widget'),
-    //   '#title_display' => 'invisible',
-    //   '#markup' => '<h2>Customer Service Use Only</h2><div><strong>You are viewing this form as an employee. To submit a report as a community member, please log out.</strong></div>',
-    // ];
-    // $element['employee_email'] = [
-    //   '#type' => 'textfield',
-    //   '#title' => t('Employee Email'),
-    //   '#id' => 'employee_email',
-    //   '#value' => $currentUserName . ' <[' . $currentUserEmail . ']>',
-    // ];
-    // $element['zendesk_request_number'] = [
-    //   '#type' => 'number',
-    //   '#title' => t('Zendesk Request Number'),
-    //   '#id' => 'zendesk_request_number',
-    //   '#description' => 'If you are completing this webform on behalf of a community member, please enter the Zendesk request number of the request created to track the interaction. In addition to creating a new request for this report, the existing interaction request will be updated and linked.',
-    // ];
-    // $element['employee_notes_panel'] = [
-    //   '#type' => 'details',
-    //   '#title' => 'Employee Notes',
-    //   '#format' => 'details-closed',
-    // ];
-    // $element['employee_notes_panel']['employee_notes'] = [
-    //   '#type' => 'textarea',
-    //   '#title' => 'Employee Notes',
-    //   '#title_display' => 'invisible',
-    //   '#description' => 'Use this area as a scratch pad or as a field to add additional notes to the request. Anything submitted in this field will be included in the request description and may be visible to the requester.'
-    // ];
-    // $element['escalate_issue'] = [
-    //   '#type' => 'checkbox',
-    //   '#title' => t('Escalate this issue'),
-    //   '#id' => 'escalate_issue'
-    // ];
-    // $element['test_submission'] = [
-    //   '#type' => 'checkbox',
-    //   '#title' => t('Test Submission'),
-    //   '#id' => 'test_submission',
-    //   '#description' => 'For administrtor use only. Handlers can be configured to process form submissions differently based on whether this box is checked. Typically configured to place tickets in the Developer Test Group in Zendesk.',
-    //   '#access_create_roles' => ['administrator'],
-    //   '#access_update_roles' => ['administrator'],
-    //   '#access_view_roles' => ['administrator'],
-    // ];
+    // Add select list for style options.
+    $element['style'] = [
+      '#type' => 'select',
+      '#title' => t('Info Style'),
+      '#options' => [
+        'option1' => t('Option 1'),
+        'option2' => t('Option 2'),
+        'option3' => t('Option 3'),
+      ],
+    ];
 
     return $element;
   }
--- web/modules/custom/portland/modules/portland_zendesk/src/Plugin/WebformElement/PortlandInfoBlockWidget.php
+++ web/modules/custom/portland/modules/portland_zendesk/src/Plugin/WebformElement/PortlandInfoBlockWidget.php
@@ -2,77 +2,26 @@
 
 namespace Drupal\portland_zendesk\Plugin\WebformElement;
 
-use Drupal\webform\Plugin\WebformElement\WebformCompositeBase;
-use Drupal\webform\WebformSubmissionInterface;
+use Drupal\webform\Plugin\WebformElement\Textarea;
 
 /**
- * Provides a 'portland_info_block_widget' element.
+ * Provides a 'custom_markup' element.
  *
  * @WebformElement(
  *   id = "portland_info_block_widget",
- *   label = @Translation("Portland info block widget"),
- *   description = @Translation("Provides a markup element used to provide additional information or warnings to webform users."),
- *   category = @Translation("Composite elements"),
- *   multiline = TRUE,
- *   composite = TRUE,
- *   states_wrapper = TRUE,
+ *   label = @Translation("Portland Info Block Widget"),
+ *   description = @Translation("Provides an information block for webforms with pre-defined formatting and style options."),
+ *   category = @Translation("Markup elements"),
  * )
- *
- * @see \Drupal\portland_zendesk\Element\PortlandInfoBlockWidget
- * @see \Drupal\webform\Plugin\WebformElement\WebformCompositeBase
- * @see \Drupal\webform\Plugin\WebformElementBase
- * @see \Drupal\webform\Plugin\WebformElementInterface
- * @see \Drupal\webform\Annotation\WebformElement
  */
-class PortlandInfoBlockWidget extends WebformCompositeBase {
+class PortlandInfoBlockWidget extends Textarea {
 
   /**
    * {@inheritdoc}
    */
-  protected function formatHtmlItemValue(array $element, WebformSubmissionInterface $webform_submission, array $options = []) {
-    $value = $this->getValue($element, $webform_submission, $options);
-
-    // this text string is used as a display value for the field value, and is what is returned by the parent
-    // level token, such as [webform_submission:values:location]. If more granular field sub-field values are
-    // needed, such as in a handler that is sending data to an external system, the sub-field needs to be
-    // specified in the token, such as [webform_submission:values:location:place_name].
-    $lines = [];
-    $line = "<h2>Customer Service Details</h2>";
-    if (isset($value['employee_email']) && $value['employee_email']) {
-      $line .= 'Form submitted by: ' . $value['employee_email'];
-    }
-    if (isset($value['zendesk_request_number']) && $value['zendesk_request_number']) {
-      $line .= 'Zendesk request number: ' . $value['zendesk_request_number'];
-    }
-    if (isset($value['employee_notes_panel']['employee_notes']) && $value['employee_notes_panel']['employee_notes']) {
-      $line .= 'Employee notes: ' . $value['employee_notes_panel']['employee_notes'];
-    }
-    $lines[] = $line;
-    return $lines;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function formatTextItemValue(array $element, WebformSubmissionInterface $webform_submission, array $options = []) {
-    $value = $this->getValue($element, $webform_submission, $options);
-
-    // this text string is used as a display value for the field value, and is what is returned by the parent
-    // level token, such as [webform_submission:values:location]. If more granular field sub-field values are
-    // needed, such as in a handler that is sending data to an external system, the sub-field needs to be
-    // specified in the token, such as [webform_submission:values:location:place_name].
-    $lines = [];
-    $lines[] = "Customer Service Details:";
-    if (isset($value['employee_email']) && $value['employee_email']) {
-      $lines[] = 'Form submitted by: ' . $value['employee_email'];
-    }
-    if (isset($value['zendesk_request_number']) && $value['zendesk_request_number']) {
-      $lines[] = 'Zendesk request number: ' . $value['zendesk_request_number'];
-    }
-    if (isset($value['employee_notes_panel']['employee_notes']) && $value['employee_notes_panel']['employee_notes']) {
-      $line .= 'Employee notes: ' . $value['employee_notes_panel']['employee_notes'];
-    }
-    return $lines;
+  public function render(array $element, array $value_callback = NULL, $filter = NULL) {
+    $element['#markup'] = "<p><strong>XXXXX</strong></p>";// $this->configuration['markup'];
+    return parent::render($element, $value_callback, $filter);
   }
 
 }
