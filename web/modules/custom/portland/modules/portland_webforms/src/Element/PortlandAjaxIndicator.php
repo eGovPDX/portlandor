0a06d72ed30776337e75c67ad5e5e10405518d98
Greg Clapp <gregory.clapp@portlandoregon.gov>
2024-09-23 16:50:36 -0700

    SIT-1185 renamed spinner image, deleted unneeded files

--- web/modules/custom/portland/modules/portland_webforms/src/Element/PortlandAjaxIndicator.php
+++ web/modules/custom/portland/modules/portland_webforms/src/Element/PortlandAjaxIndicator.php
@@ -26,7 +26,7 @@ class PortlandAjaxIndicator extends WebformCompositeBase
       '#type' => 'markup',
       '#title' => 'Ajax Progress Indicator',
       '#title_display' => 'invisible',
-      '#markup' => '<div id="ajax-status"><img src="/modules/custom/portland/modules/portland_webforms/images/loading.png" alt="Loading..."></div>',
+      '#markup' => '<div id="ajax-status"><img src="/modules/custom/portland/modules/portland_webforms/images/loading-spinner.png" alt="Loading..."></div>',
     ];
     return $element;
   }
