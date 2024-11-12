a810931e3e1ce369a3dabaf3d5fea51a60b1f61f
Oden <hello@odensc.com>
2024-11-11 22:47:08 -0800

    PGOV-1003 add once() to other scripts

--- web/themes/custom/cloudy/src/js/behaviors/notification.js
+++ web/themes/custom/cloudy/src/js/behaviors/notification.js
@@ -1,29 +1,25 @@
 const COOKIE_PREFIX = "Drupal.visitor.cloudy_notification_dismissed.";
 
-Drupal.behaviors.notificationHandler = {
-  attach() {
-    const notifications = document.querySelectorAll(".cloudy-notification");
-    notifications.forEach((notification) => {
-      const notificationId = notification.dataset["nid"];
-      const notificationChanged = notification.dataset["changed"];
-      const cookieStr =
-        COOKIE_PREFIX + notificationId + "=" + notificationChanged;
+Drupal.behaviors.cloudyNotificationHandler = {
+	attach(context) {
+		once("cloudyNotificationHandler", ".cloudy-notification", context).forEach((notification) => {
+			const notificationId = notification.dataset["nid"];
+			const notificationChanged = notification.dataset["changed"];
+			const cookieStr = COOKIE_PREFIX + notificationId + "=" + notificationChanged;
 
-      // If we haven't found a dismissal cookie for the notification and changed timestamp,
-      // show the notification.
-      if (!document.cookie.includes(cookieStr)) {
-        notification.classList.add("cloudy-notification--active-dismissible");
-      }
+			// If we haven't found a dismissal cookie for the notification and changed timestamp,
+			// show the notification.
+			if (!document.cookie.includes(cookieStr)) {
+				notification.classList.add("cloudy-notification--active-dismissible");
+			}
 
-      const closeButton = notification.querySelector(
-        ".cloudy-notification__close"
-      );
-      closeButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        notification.classList.remove("cloudy-notification--active-dismissible");
-        // Set dismissal cookie
-        document.cookie = cookieStr;
-      });
-    });
-  },
+			const closeButton = notification.querySelector(".cloudy-notification__close");
+			closeButton.addEventListener("click", (e) => {
+				e.preventDefault();
+				notification.classList.remove("cloudy-notification--active-dismissible");
+				// Set dismissal cookie
+				document.cookie = cookieStr;
+			});
+		});
+	},
 };
