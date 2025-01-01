a36a68a0f7ac7711c4f15a30b21b041ef423e5ab
Oden <hello@odensc.com>
2024-12-31 16:19:27 -0800

    PGOV-1270 fix error if no referenceable electeds

--- web/modules/custom/portland/modules/portland_ecouncil/src/Plugin/Field/FieldWidget/CouncilVotesWidget.php
+++ web/modules/custom/portland/modules/portland_ecouncil/src/Plugin/Field/FieldWidget/CouncilVotesWidget.php
@@ -74,7 +74,7 @@ class CouncilVotesWidget extends WidgetBase implements ContainerFactoryPluginInt
     $bundle_field_definitions = $this->entityFieldManager->getFieldDefinitions('relation', 'council_vote');
     $selection_handler = $this->selectionManager->getSelectionHandler($bundle_field_definitions['field_council_member']);
     $referenceable_entities = $selection_handler->getReferenceableEntities();
-    $options = $referenceable_entities['elected_official'];
+    $options = $referenceable_entities['elected_official'] ?? [];
 
     // Find any existing votes with old elected officials and add them as an option,
     // so the editor can see and edit/delete them.
