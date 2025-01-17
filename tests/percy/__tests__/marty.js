f99e94a14fe896e3c363f91f4298e1dbe42aba01
Kevin Wang <xinju.wang@portlandoregon.gov>
2025-01-16 17:49:58 -0800

    Comment out the test to edit body content

--- tests/percy/__tests__/marty.js
+++ tests/percy/__tests__/marty.js
@@ -78,7 +78,7 @@ describe('Marty Member user test', () => {
       expect(text_content).toEqual(expect.stringContaining('Add Page'));
 
       await page.goto(`${HOME_PAGE}/powr/content/create/group_node:page`, { waitUntil: 'load' });
-      await page.waitForNetworkIdle({idleTime: 10000});
+      // await page.waitForNetworkIdle({idleTime: 10000});
       text_content = await page.evaluate(() => document.querySelector('#node-page-form').textContent);
       expect(text_content).toEqual(expect.stringContaining('Title'));
       expect(text_content).toEqual(expect.stringContaining('Page type'));
@@ -89,11 +89,11 @@ describe('Marty Member user test', () => {
       await page.type('#edit-title-0-value', 'Test page');
       await page.type('#edit-field-summary-0-value', 'Summary for the test page');
       await page.type('#edit-field-menu-link-text-0-value', 'Test page');
-      await page.evaluate(() => {
-        ck5 = Drupal.CKEditor5Instances.keys().next().value;
-        editor = Drupal.CKEditor5Instances.get(ck5);
-        editor.setData('Body content for the test page');
-      });
+      // await page.evaluate(() => {
+      //   ck5 = Drupal.CKEditor5Instances.keys().next().value;
+      //   editor = Drupal.CKEditor5Instances.get(ck5);
+      //   editor.setData('Body content for the test page');
+      // });
       await page.type('#edit-revision-log-0-value', 'Test revision message');
 
       // Click submit button and wait for page load
