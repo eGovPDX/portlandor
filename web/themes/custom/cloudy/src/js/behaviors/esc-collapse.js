618c250233063be89c64827315758458cca29f46
Oden <hello@odensc.com>
2025-05-12 18:36:05 -0700

    PGOV-1552 collapse with esc key when trigger element is focused, and return focus

--- web/themes/custom/cloudy/src/js/behaviors/esc-collapse.js
+++ web/themes/custom/cloudy/src/js/behaviors/esc-collapse.js
@@ -2,6 +2,8 @@ import Collapse from "bootstrap/js/src/collapse";
 
 /**
  * Add a data-cloudy-esc-collapse attribute to any collapsible element to close it when the escape key is pressed if it has focus.
+ * ASSUMPTION: Should only be used with collapse elements that only have one trigger element,
+ *             since the first trigger element will be used to return focus.
  */
 Drupal.behaviors.cloudyEscCollapse = {
   attach(context) {
@@ -9,7 +11,15 @@ Drupal.behaviors.cloudyEscCollapse = {
     document.body.addEventListener("keydown", (e) => {
       if (e.key === "Escape") {
         elements.forEach((el) => {
-          if (el.matches(":focus-within")) Collapse.getInstance(el).hide();
+          const collapse = Collapse.getInstance(el);
+          if (
+            el.matches(":focus-within") ||
+            collapse._triggerArray?.[0]?.matches(":focus-within")
+          ) {
+            collapse._triggerArray?.[0]?.focus();
+
+            collapse.hide();
+          }
         });
       }
     });
