fc5f199b26b36f5ee7a53b68cd9d9b5c989d1009
Richard Davies <richard.davies@portlandoregon.gov>
2025-07-23 14:25:21 -0700

    PGOV-1685 Delete orphaned group content.

--- web/modules/custom/portland/src/Drush/Commands/PortlandCommands.php
+++ web/modules/custom/portland/src/Drush/Commands/PortlandCommands.php
@@ -4,13 +4,15 @@ namespace Drupal\portland\Drush\Commands;
 
 use Drush\Attributes as CLI;
 use Drush\Commands\DrushCommands;
+use Drupal\group\Entity\GroupRelationship;
 
 class PortlandCommands extends DrushCommands {
   /**
    * Drush command to reset the user sync process so the next cron run will restart the sync process.
+   * @command portland:reset_user_sync
+   * @aliases pgov-reset-user-sync
+   * @description Reset the user sync process so the next cron run will restart the sync process
    */
-  #[CLI\Command(name: 'portland:reset_user_sync')]
-  #[CLI\Usage(name: 'portland:reset_user_sync', description: 'Reset the user sync process')]
   public function reset_user_sync() {
     // Clear all items in the queue
     /** @var QueueFactory $queue_factory */
@@ -32,5 +34,39 @@ class PortlandCommands extends DrushCommands {
     ]);
 
     echo "The user sync process will start in the next cron run." . PHP_EOL;
+  }
+
+
+  /**
+   * Drush command to delete orphaned nodes belonging to deleted groups.
+   * @command portland:delete_orphaned_content
+   * @aliases pgov-delete-orphaned-content
+   * @description Deletes content still assigned to a group that no longer exists.
+   */
+  public function delete_orphaned_content() {
+    // Load all nodes
+    $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+    $nids = $node_storage->getQuery()->accessCheck(FALSE)->execute();
+    
+    foreach ($nids as $nid) {
+      // Load the node and check its group content relationships.
+      $node = $node_storage->load($nid);
+      $group_contents = GroupRelationship::loadByEntity($node);
+      foreach ($group_contents as $group_content) {
+        // If the group no longer exists, remove the group content.
+        if ($group_content->getGroup() === NULL) {
+          $nodeUrl = $node->toUrl('canonical', ['absolute' => TRUE])->toString();
+          echo "Deleting group content: $nodeUrl" . PHP_EOL;
+          $group_content->delete();
+
+          // If there was only one group content, delete the orphaned node.
+          if (count($group_contents) === 1) {
+            echo "Deleting orphaned node: $nodeUrl" . PHP_EOL;
+            $node->delete();
+          }
+        }
+      }
+    } 
+    echo "Done deleting orphaned group content." . PHP_EOL;
   }
 }
