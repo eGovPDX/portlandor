11d93e0418b8de77bf727c4fabee12525fdd5b09
Oden <hello@odensc.com>
2025-08-11 19:19:12 -0700

    add caching to view select plugin

--- web/modules/custom/portland/modules/portland_webforms/src/Plugin/WebformElement/PortlandViewsSelectTrait.php
+++ web/modules/custom/portland/modules/portland_webforms/src/Plugin/WebformElement/PortlandViewsSelectTrait.php
@@ -6,6 +6,8 @@ use Drupal\views\Views;
 use Drupal\webform\Utility\WebformOptionsHelper;
 
 trait PortlandViewsSelectTrait {
+  private const CACHE_PREFIX = 'portland_webforms:views_select:';
+
   public static function setOptions(array &$element, array $settings = []) {
     if (!empty($element['#options'])) {
       return;
@@ -16,22 +18,35 @@ trait PortlandViewsSelectTrait {
     $label_field = $element['#label_field'] ?? '';
     $value_field = $element['#value_field'] ?? '';
 
+    $cache = \Drupal::cache();
+    $cache_id = self::CACHE_PREFIX . hash('sha256', $view_id . $display_id . $label_field . $value_field);
+
     $options = [];
     if ($view_id && $display_id && $label_field && $value_field) {
-      $view = Views::getView($view_id);
-      if ($view && $view->access($display_id)) {
-        $view->setDisplay($display_id);
-        $view->preExecute();
-        $view->render();
-        $label_field_handler = $view->field[$label_field];
-        $value_field_handler = $view->field[$value_field];
-        foreach ($view->result as $row_index => $row) {
-          $label = str_replace("\n", '', $label_field_handler->tokenizeValue((string) $label_field_handler->render($row), $row_index));
-          $value = html_entity_decode($value_field_handler->tokenizeValue((string) $value_field_handler->render($row), $row_index));
-          if ($value !== '') {
-            $options[$value] = $label;
+      $cache_result = $cache->get($cache_id);
+      if ($cache_result && !empty($cache_result->data)) {
+        $options = $cache_result->data;
+      } else {
+        $view = Views::getView($view_id);
+        if ($view && $view->access($display_id)) {
+          $view->setDisplay($display_id);
+          $view->preExecute();
+          $view->render();
+          $label_field_handler = $view->field[$label_field];
+          $value_field_handler = $view->field[$value_field];
+          foreach ($view->result as $row_index => $row) {
+            $label = str_replace("\n", '', $label_field_handler->tokenizeValue((string) $label_field_handler->render($row), $row_index));
+            $value = html_entity_decode($value_field_handler->tokenizeValue((string) $value_field_handler->render($row), $row_index));
+            if ($value !== '') {
+              $options[$value] = $label;
+            }
           }
+
+          \Drupal::service('renderer')->addCacheableDependency($element, $view->storage);
         }
+
+        // Cache the options for a short period (10 seconds) to avoid repeated queries when multiple elements in one form are using the same view.
+        $cache->set($cache_id, $options, time() + 10);
       }
     }
 
@@ -39,6 +54,5 @@ trait PortlandViewsSelectTrait {
     $options = WebformOptionsHelper::decodeOptions($options);
 
     $element['#options'] = $options;
-    \Drupal::service('renderer')->addCacheableDependency($element, $view->storage);
   }
 }
