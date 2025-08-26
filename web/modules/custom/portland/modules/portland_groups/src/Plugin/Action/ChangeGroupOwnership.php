983063998f1dd0153d3c3ef712c75c939ce9d4bc
Kevin Wang <xinju.wang@portlandoregon.gov>
2025-08-25 17:12:09 -0700

    Rename action. Show warning message. Add the action to group content admin view.

--- web/modules/custom/portland/modules/portland_groups/src/Plugin/Action/ChangeGroupOwnership.php
+++ web/modules/custom/portland/modules/portland_groups/src/Plugin/Action/ChangeGroupOwnership.php
@@ -4,10 +4,14 @@ namespace Drupal\portland_groups\Plugin\Action;
 
 use Drupal\Core\Action\Attribute\Action;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\PluginFormInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\RevisionableInterface;
+use Drupal\Core\Entity\RevisionLogInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\groupmedia\AttachMediaToGroup;
 use Drupal\media\MediaInterface;
@@ -18,11 +22,11 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * Move media to a different group.
  */
 #[Action(
-  id: 'vbo_move_group_media',
-  label: new TranslatableMarkup('Move media to a different group (custom action)'),
-  type: 'media',
+  id: 'vbo_change_group_ownership',
+  label: new TranslatableMarkup('Change the group ownership of content or media (custom action)'),
+  type: '', // Leave this blank to support both node and media.
 )]
-class MoveGroupMedia extends ViewsBulkOperationsActionBase implements PluginFormInterface, ContainerFactoryPluginInterface
+class ChangeGroupOwnership extends ViewsBulkOperationsActionBase implements PluginFormInterface, ContainerFactoryPluginInterface
 {
   /**
    * Entity type manager.
@@ -81,33 +85,52 @@ class MoveGroupMedia extends ViewsBulkOperationsActionBase implements PluginForm
   /**
    * {@inheritdoc}
    */
-  public function execute(?MediaInterface $media = NULL)
+  public function execute(?FieldableEntityInterface $entity = NULL)
   {
-    if (empty($media)) {
-      return $this->t('No media specified.');
+    if (empty($entity)) {
+      return $this->t('No entity specified.');
     }
 
-    $old_group_id = $media->get('field_display_groups')->target_id;
+    if (!$entity->hasField('field_display_groups')) {
+      return $this->t('Entity does not have a Displayed In Group field.');
+    }
+
+    $old_group_id = $entity->get('field_display_groups')->target_id;
     $new_group_id = $this->configuration['group_id'];
-    $new_group = \Drupal::entityTypeManager()->getStorage('group')->load($new_group_id);
-    $new_group_title = $new_group ? $new_group->label() : '';
-
-    // Update the Displayed In Group field on the media entity.
-    if ($media->hasField('field_display_groups')) {
-      $old_display_groups = $media->get('field_display_groups')->getValue();
-      // Filter out the old group and the new group from the list.
-      $new_display_groups = array_filter($old_display_groups, function ($old_display_group) use ($old_group_id, $new_group_id) {
-        return ($old_display_group['target_id'] != $old_group_id && $old_display_group['target_id'] != $new_group_id);
-      });
-      // Add the new group to the beginning of the list. The first group in the list is the primary group.
-      array_unshift($new_display_groups, ['target_id' => $new_group_id]);
-      $media->set('field_display_groups', $new_display_groups);
-      $media->setRevisionLogMessage("Moved to group by bulk operation: $new_group_title");
-      $media->save();
-      // Group relationships are automatically synced after the media is saved. 
-      // See _portland_groups_synchronize_entity_group_ownership() in portland_groups.module.
-      return $this->t("Media moved to " . $new_group_title);
+    if ($old_group_id == $new_group_id) {
+      return $this->t('Entity is already in the selected group.');
     }
+
+    $old_group = \Drupal::entityTypeManager()->getStorage('group')->load($old_group_id);
+    if (empty($old_group)) {
+      return $this->t("Cannot find the original primary group with ID: $old_group_id");
+    }
+    $old_group_title = $old_group->label();
+    $new_group = \Drupal::entityTypeManager()->getStorage('group')->load($new_group_id);
+    if (empty($new_group)) {
+      return $this->t("Cannot find the destination primary group with ID: $new_group_id");
+    }
+    $new_group_title = $new_group->label();
+
+    // Update the Displayed In Group field on the entity.
+    $old_display_groups = $entity->get('field_display_groups')->getValue();
+    // Filter out the old group and the new group from the list.
+    $new_display_groups = array_filter($old_display_groups, function ($old_display_group) use ($old_group_id, $new_group_id) {
+      return ($old_display_group['target_id'] != $old_group_id && $old_display_group['target_id'] != $new_group_id);
+    });
+    // Add the new group to the beginning of the list. The first group in the list is the primary group.
+    array_unshift($new_display_groups, ['target_id' => $new_group_id]);
+    $entity->set('field_display_groups', $new_display_groups);
+    if ($entity instanceof RevisionableInterface) {
+      $entity->setNewRevision(TRUE);
+    }
+    if ($entity instanceof RevisionLogInterface) {
+      $entity->setRevisionLogMessage("Bulk operation changed group ownership from $old_group_title ($old_group_id) to $new_group_title ($new_group_id).");
+    }
+    $entity->save();
+    // Group relationships are automatically synced after the media is saved. 
+    // See _portland_groups_synchronize_entity_group_ownership() in portland_groups.module.
+    return $this->t("Changed group ownership to " . $new_group_title);
   }
 
   /**
@@ -170,9 +193,17 @@ class MoveGroupMedia extends ViewsBulkOperationsActionBase implements PluginForm
    */
   public function access($object, ?AccountInterface $account = NULL, $return_as_object = FALSE)
   {
-    /** @var \Drupal\media\MediaInterface $object */
-    $result = $object->access('update', $account, TRUE);
-    return $return_as_object ? $result : $result->isAllowed();
+    if ($object instanceof \Drupal\Core\Entity\EntityInterface) {
+      $entity_type = $object->getEntityTypeId();
+      $allowed_types = ['node', 'media'];
+
+      if (in_array($entity_type, $allowed_types)) {
+        // Check update access for the entity.
+        return $object->access('update', $account, $return_as_object);
+      }
+    }
+
+    return $return_as_object ? AccessResult::forbidden() : FALSE;
   }
 
   /**
