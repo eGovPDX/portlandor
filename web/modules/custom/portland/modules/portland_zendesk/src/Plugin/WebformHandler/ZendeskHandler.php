a031c2c741e4a36f79324898d5893711a5e6bafb
Greg Clapp <gregory.clapp@portlandoregon.gov>
2025-10-31 15:07:41 -0700

    SITU-1217 manually revert ZendeskHandler.php

--- web/modules/custom/portland/modules/portland_zendesk/src/Plugin/WebformHandler/ZendeskHandler.php
+++ web/modules/custom/portland/modules/portland_zendesk/src/Plugin/WebformHandler/ZendeskHandler.php
@@ -17,6 +17,7 @@ use Drupal\webform\WebformTokenManagerInterface;
 use Drupal\Core\Serialization\Yaml;
 use Drupal\file\Entity\File;
 use Drupal\portland_zendesk\Utils\Utility;
+use Drupal\Core\File\FileSystemInterface;
 
 /**
  * Form submission to Zendesk handler.
@@ -791,7 +792,16 @@ class ZendeskHandler extends WebformHandlerBase
     // get all webform elements
     $elements = $this->getWebform()->getElementsInitializedAndFlattened();
 
+    $lock = \Drupal::lock();
+    $sid  = $webform_submission->id();
+    $key  = 'zendesk_send:' . ($sid ?: $webform_submission->uuid());
+
+    if (!$lock->acquire($key, 30)) {
+      throw new \RuntimeException('Duplicate submission in progress.');
+    }
+
     // attempt to send request to create zendesk ticket
+    $__temp_paths = [];
     try {
       // initiate api client
       $client = new ZendeskClient();
@@ -833,10 +843,12 @@ class ZendeskHandler extends WebformHandlerBase
               $request['comment']['uploads'] = [];
             }
 
-            if ($element) $filename = $this->transformFilename($file->getFilename(), $element, $webform_submission);;
-            // upload file and get response
+            if ($element) $filename = $this->transformFilename($file->getFilename(), $element, $webform_submission);
+
+            $path = $this->pathForZendeskUpload($file);   // new helper below
+            $__temp_paths[] = $path;                      // remember to clean up
             $attachment = $client->attachments()->upload([
-              'file' => $file->getFileUri(),
+              'file' => $path,                             // real path, not private://
               'type' => $file->getMimeType(),
               'name' => $filename,
             ]);
@@ -867,6 +879,10 @@ class ZendeskHandler extends WebformHandlerBase
         '@message' => $message,
         'link' => $this->getWebform()->toLink($this->t('Edit'), 'handlers')->toString(),
       ]);
+    } finally {
+      // always remove any temporary copies we created
+      $this->cleanupTempUploads($__temp_paths);
+      $lock->release($key);
     }
 
     return $new_ticket_id;
