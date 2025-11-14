f1f556e1150dc01ac2f3d4e9569fe6c9e2cb155c
Kevin Wang <xinju.wang@portlandoregon.gov>
2025-11-13 18:10:54 -0800

    Update settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -849,7 +849,25 @@ $settings['state_cache'] = TRUE;
  *      a local development environment, to insure that
  *      the site settings remain consistent.
  */
-include __DIR__ . "/settings.pantheon.php";
+/**
+ * Determine whether this is a preproduction or production environment, and
+ * then load the pantheon services.yml file.  This file should be named either
+ * 'pantheon-production-services.yml' (for 'live' or 'test' environments)
+ * 'pantheon-preproduction-services.yml' (for 'dev' or multidev environments).
+ */
+$pantheon_services_file = __DIR__ . '/services.pantheon.preproduction.yml';
+if (
+  isset($_ENV['PANTHEON_ENVIRONMENT']) &&
+  ( ($_ENV['PANTHEON_ENVIRONMENT'] == 'live') || ($_ENV['PANTHEON_ENVIRONMENT'] == 'test') )
+) {
+  $pantheon_services_file = __DIR__ . '/services.pantheon.production.yml';
+}
+
+if (file_exists($pantheon_services_file)) {
+  $settings['container_yamls'][] = $pantheon_services_file;
+}
+
+include \Pantheon\Integrations\Assets::dir() . "/settings.pantheon.php";
 
 /**
  * If there is a local settings file, then include it
