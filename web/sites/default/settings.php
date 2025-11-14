dbc72b750a4d87f69b0ba276d9d39d0acfda11aa
Kevin Wang <xinju.wang@portlandoregon.gov>
2025-11-13 17:09:47 -0800

    Tweak settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -996,10 +996,9 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT']) && $_ENV['PANTHEON_ENVIRONMENT'] !== 'l
   $settings['cache_prefix']['default'] = 'pantheon-redis';
 
   $settings['cache']['bins']['form'] = 'cache.backend.database'; // Use the database for forms
-  $settings['cache']['bins']['bootstrap'] = 'cache.backend.redis';
-  $settings['cache']['bins']['render'] = 'cache.backend.redis';
-  $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.redis';
-  $settings['cache']['bins']['page'] = 'cache.backend.redis';
+  $settings['cache']['bins']['bootstrap'] = 'cache.backend.memory';
+  $settings['cache']['bins']['render'] = 'cache.backend.memory';
+  $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.memory';
   
   // Apply changes to the container configuration to make better use of Redis.
   // This includes using Redis for the lock and flood control systems, as well
