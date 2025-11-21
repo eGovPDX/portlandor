97cabe43f5060be75e1d5df8ab42ff616add99c1
Greg Clapp <gregory.clapp@portlandoregon.gov>
2025-11-21 13:24:12 -0800

    PGOV-1823 fixed some error texgt

--- web/modules/custom/portland/modules/portland_address_verifier/js/addressVerifierView.js
+++ web/modules/custom/portland/modules/portland_address_verifier/js/addressVerifierView.js
@@ -22,7 +22,7 @@ function AddressVerifierView(jQuery, element, model, settings) {
 // var $suggestModal;
 // var $statusModal;
 const MUST_PROVIDE_ADDRESS_MESSAGE = "You must enter an address or partial address to verify.";
-const UNVERIFIED_WARNING_MESSAGE = "We're unable to verify this address. If you're certain this is the full, correct address, verification_required."
+const UNVERIFIED_WARNING_MESSAGE = "We're unable to verify this address. If you're certain this is the full correct address, you may proceed."
 const VERFICATION_REQUIRED_MESSAGE = "Address verification is required, but we're unable to verify this address. Please try again.";
 const VERIFIED_MESSAGE = "Address found!";
 const SERVER_ERROR_MESSAGE = "There was an problem connecting to our location services. Please check the <a href=\"/\" target=\"_blank\">Portland.gov homepage</a> for maintenance or outage alerts, or try again later.";
