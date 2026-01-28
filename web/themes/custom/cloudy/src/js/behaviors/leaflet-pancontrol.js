04edc93ce20a1a4610ffbe70bd465cc177b530e0
Kevin Wang <xinju.wang@portlandoregon.gov>
2026-01-28 15:52:34 -0800

    Add pan control and help for keybord navigation

--- web/themes/custom/cloudy/src/js/behaviors/leaflet-pancontrol.js
+++ web/themes/custom/cloudy/src/js/behaviors/leaflet-pancontrol.js
@@ -1,13 +1,9 @@
 Drupal.behaviors.leafletPanControl = {
-  attach: function(context, settings) {
-    if (!settings.leaflet) {
-      return;
-    }
-
-    jQuery.each(settings.leaflet, function(mapid, map_settings) {
-      const map = map_settings.lMap;
+  attach: function(context) {
+    jQuery(context).on('leafletMapInit', function (e, settings, map) {
       if (map !== undefined && !map._panControlAdded) {
         window.L.control.pan({ position: 'bottomleft' }).addTo(map);
+        console.log('Adding pan control to map: ', settings.id);
         map._panControlAdded = true;
       }
     });
