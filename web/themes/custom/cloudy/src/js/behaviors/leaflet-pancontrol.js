9207dfcc91ba78f4ec91b398f310204da7b68194
Kevin Wang <xinju.wang@portlandoregon.gov>
2026-01-29 10:45:11 -0800

    Update log message

--- web/themes/custom/cloudy/src/js/behaviors/leaflet-pancontrol.js
+++ web/themes/custom/cloudy/src/js/behaviors/leaflet-pancontrol.js
@@ -3,7 +3,7 @@ Drupal.behaviors.leafletPanControl = {
     jQuery(context).on('leafletMapInit', function (e, settings, map) {
       if (map !== undefined && !map._panControlAdded) {
         window.L.control.pan({ position: 'bottomleft' }).addTo(map);
-        console.log('Adding pan control to map: ', settings.id);
+        console.log('Added pan control to map: ', settings.id);
         map._panControlAdded = true;
       }
     });
