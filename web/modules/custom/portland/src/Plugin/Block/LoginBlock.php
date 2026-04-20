21f3f1eaa1bfc1c06c4dcdc1b44706ff57d8185e
Kevin Wang <xinju.wang@portlandoregon.gov>
2026-04-20 11:57:19 -0700

    PGOV-2051 Fix the query string on the logout link

--- web/modules/custom/portland/src/Plugin/Block/LoginBlock.php
+++ web/modules/custom/portland/src/Plugin/Block/LoginBlock.php
@@ -38,12 +38,15 @@ class LoginBlock extends BlockBase {
       if(empty($destination)) {
         $request->query->set("destination", $request->getPathInfo());
       }
-      $query_string = http_build_query($request->query->all());
       $login_link = "";
       if ($logged_in) {
+        $query_string = http_build_query($request->query->all() + [
+          'csrf_token' => \Drupal::service('csrf_token')->get('user.logout'),
+        ]);
         $login_text = $this->t('Editor log out');
         $login_link = "<a href=\"/user/logout?$query_string\">$login_text</a>";
       } else {
+        $query_string = http_build_query($request->query->all());
         $login_text = $this->t('Editor log in');
         $login_link = "<a href=\"/user/login?$query_string\">$login_text</a>";
       }
